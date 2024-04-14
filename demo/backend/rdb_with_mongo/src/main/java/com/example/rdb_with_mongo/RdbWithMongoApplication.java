ea9792deb40c7882555f0d64769cbf7c97dc2ff8
김도현 <nanayagoon@daum.net>
2024-04-14 16:32:36 +0900

    Init : RDB 와 MongoDB 연동

---
+++ demo/backend/rdb_with_mongo/src/main/java/com/example/rdb_with_mongo/RdbWithMongoApplication.java
@@ -0,0 +1,15 @@
+package com.example.rdb_with_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdbWithMongoApplication {
+
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(RdbWithMongoApplication.class, args);
+    }
+
+}
