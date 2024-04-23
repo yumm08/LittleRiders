f1520a595dd462d0179338189d6ebb6aeadc0c41
김민준 <alswns1285@gmail.com>
2024-04-23 16:48:06 +0900

    Fix: [FE] 메뉴 리스트 key 속성 추가

--- Frontend/src/layouts/Header/HeaderMenuList.tsx
+++ Frontend/src/layouts/Header/HeaderMenuList.tsx
@@ -4,8 +4,8 @@ import HeaderMenuListItem from '@layouts/Header/HeaderMenuListItem'
 export default function HeaderMenuList() {
   return (
     <div className="flex h-1/2 w-full items-center gap-24">
-      {MENU_LIST.map(({ text, path }) => (
-        <HeaderMenuListItem text={text} path={path} />
+      {MENU_LIST.map(({ text, path }, index) => (
+        <HeaderMenuListItem key={index} text={text} path={path} />
       ))}
     </div>
   )
