745c0b32daadfc4919edd4be13110a31f3ea8416
김민준 <alswns1285@gmail.com>
2024-04-23 16:20:47 +0900

    Feat: [FE] 라우터 설정
    S10P31A405-163
    
    - 홈(/home)
    - 원생 관리(/manage/child)
    - 배차 관리(/manage/dispatch)
    - 운영 관리(/manage/oprate)
    - 마이페이지(/mypage)

---
+++ Frontend/src/pages/OperatePage/index.tsx
@@ -0,0 +1,3 @@
+export default function OperatePage() {
+  return <div>OperatePage</div>
+}
---
+++ Frontend/src/routes/router.tsx
@@ -0,0 +1,48 @@
+import ChildPage from '@pages/ChildPage'
+import DispatchPage from '@pages/DispatchPage'
+import HomePage from '@pages/HomePage'
+import MyPage from '@pages/MyPage'
+import OperatePage from '@pages/OperatePage'
+
+import App from 'App'
+import { createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'home',
+        element: <HomePage />,
+      },
+      {
+        path: 'manage',
+        children: [
+          {
+            path: 'child',
+            element: <ChildPage />,
+          },
+          {
+            path: 'dispatch',
+            element: <DispatchPage />,
+          },
+          {
+            path: 'operate',
+            element: <OperatePage />,
+          },
+        ],
+      },
+      {
+        path: 'mypage',
+        element: <MyPage />,
+      },
+    ],
+  },
+])
+
+export default router
