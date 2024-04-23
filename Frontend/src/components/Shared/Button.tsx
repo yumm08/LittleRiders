be1ee3f5fd5f586764f7405717d9fd09e1dd941c
박희준 <wooryjoon@gmail.com>
2024-04-23 16:08:10 +0900

    Fix: [FE] 테일윈드와 타협

--- Frontend/src/components/Shared/Button.tsx
+++ Frontend/src/components/Shared/Button.tsx
@@ -1,34 +1,30 @@
-import { ColorPalette } from '@style/ColorPalette'
-
 interface Props {
   innerText: string
   full?: boolean
   onClick: React.MouseEventHandler<HTMLButtonElement>
-  color: ColorPalette
+  color?: string
 }
 /**
  * @summary 버튼 컴포넌트
  * @param innerText : 버튼 내부 텍스트
  * @param full(optional) : 레이아웃 전체 꽉 채우고 싶을 때 사용
  * @param onClick : 버튼 클릭 시 실행할 콜백함수
- * @param color : 버튼 배경 색깔 (colorPalette에 명시된 이름 삽입)
- * @returns
+ * @param color : 버튼 배경 색깔  'bg-' 형태로 주입
+ * @returnsa
  */
+
 export default function Button({
   innerText,
   full = false,
   onClick,
-  color,
+  color = 'bg-lightgreen',
 }: Props) {
   const fullWidth = full ? 'w-full' : ''
-  const bgColor = 'bg-' + color
 
+  const className = `${color} ${fullWidth} p-3 rounded-lg flex justify-center items-center`
   return (
-    <button
-      onClick={onClick}
-      className={`${bgColor} ${fullWidth} p-5 rounded-lg flex justify-center items-center`}
-    >
-      <span className="text-xl">{innerText}</span>
+    <button onClick={onClick} className={className}>
+      <span className="text-xl text-white">{innerText}</span>
     </button>
   )
 }
