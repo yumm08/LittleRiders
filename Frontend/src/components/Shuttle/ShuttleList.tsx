48b0e7306d486c1bbee074aa9681c112046118bf
박희준 <wooryjoon@gmail.com>
2024-04-24 15:16:04 +0900

    Ref: [FE] ShuttleCard 배열 더미데이터로 수정
    S10P31A405-178

--- Frontend/src/components/Shuttle/ShuttleList.tsx
+++ Frontend/src/components/Shuttle/ShuttleList.tsx
@@ -6,22 +6,14 @@ import ShuttleCard from './ShuttleCard'
 interface Props {
   show: number
 }
-
+const DUMMY = [1,2,3,4,5,6,7,8,9,10]
 export default function ShuttleList({ show }: Props) {
   return (
     <CardListContainer type="차량" onClick={() => {}}>
       <CardCarousel show={show}>
-        <ShuttleCard />
-        <ShuttleCard />
-        <ShuttleCard />
-        <ShuttleCard />
-        <ShuttleCard />
-        <ShuttleCard />
-        <ShuttleCard />
-        <ShuttleCard />
-        <ShuttleCard />
-        <ShuttleCard />
-        <ShuttleCard />
+        {DUMMY.map((id)=>{
+          return <ShuttleCard id={id} />
+        })}
       </CardCarousel>
     </CardListContainer>
   )
