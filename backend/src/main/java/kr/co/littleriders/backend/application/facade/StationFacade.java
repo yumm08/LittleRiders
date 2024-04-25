aa2486cad6829c2c25bf46070bb743f39c6ffae8
이수현 <suhyeon9120@gmail.com>
2024-04-25 16:21:51 +0900

    Feat: [BE] StationFacade 추가
    S10O31A405-169

---
+++ backend/src/main/java/kr/co/littleriders/backend/application/facade/StationFacade.java
@@ -0,0 +1,15 @@
+package kr.co.littleriders.backend.application.facade;
+
+import kr.co.littleriders.backend.application.dto.request.StationCreateRequest;
+import kr.co.littleriders.backend.application.dto.response.StationResponse;
+import kr.co.littleriders.backend.domain.academy.entity.Academy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface StationFacade {
+
+    void createStation(Academy academy, StationCreateRequest createRequest);
+
+    Page<StationResponse> getAllStationByName(String name, Academy academy, Pageable pageable);
+
+}
