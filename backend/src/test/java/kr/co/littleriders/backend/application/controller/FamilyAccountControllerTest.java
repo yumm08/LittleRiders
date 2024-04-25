e13450396df40f03993e49f6ff8e08928b9c6772
김도현 <nanayagoon@daum.net>
2024-04-25 22:16:00 +0900

    Test: [BE] 로그인 컨트롤러 테스트
    S10P31A405-199
    
    로그인시 응답 값이 헤더와 cookie 에 저장됨을 확인

--- backend/src/test/java/kr/co/littleriders/backend/application/controller/FamilyAccountControllerTest.java
+++ backend/src/test/java/kr/co/littleriders/backend/application/controller/FamilyAccountControllerTest.java
@@ -1,7 +1,10 @@
 package kr.co.littleriders.backend.application.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.co.littleriders.backend.application.dto.request.SignInRequest;
 import kr.co.littleriders.backend.application.facade.FamilyAccountFacade;
+import kr.co.littleriders.backend.global.jwt.JwtToken;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,8 +14,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -60,6 +67,41 @@ class FamilyAccountControllerTest {
                     .andDo(print());
 
         }
+    }
+
+    @Nested
+    @DisplayName("signIn 테스트")
+    class signIn {
+
+
+        @Test
+
+        @DisplayName("성공")
+        void whenSuccess() throws Exception {
+            JwtToken jwtToken = JwtToken.of("apple", 1234, "banana", 12345);
+
+            given(familyAccountFacade.signIn(any())).willReturn(
+                    jwtToken
+            );
+
+            SignInRequest signInRequest = SignInRequest.of("apple", "1234");
+
+
+            mockMvc.perform(
+                            post("/family/account/signIn")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(objectMapper.writeValueAsString(signInRequest))
+
+                    )
+                    .andExpect(status().isOk())
+                    .andExpect(header().exists("Authorization"))
+                    .andDo(print());
+
+
+        }
+
 
     }
+
+
 }
