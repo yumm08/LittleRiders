8459f2a6e376a44eb512c41b5f2f499ca3fa361e
gwanu-dev <devkwanwoo@gmail.com>
2024-04-25 17:45:25 +0900

    Feat: [FE] Page 쿼리 추가 및 httpCommons 수정
    S10P31A405-162

--- Frontend/src/pages/DispatchPage/index.tsx
+++ Frontend/src/pages/DispatchPage/index.tsx
@@ -1,3 +1,33 @@
+import { useState } from 'react'
+
+import { ItemListView, NaverMap, RouteList } from '@components/Dispatch'
+
+import { useQuery } from '@tanstack/react-query'
+
+import { getRouteList } from '@apis/dispatch'
+
+import Page from '@layouts/Page'
+
 export default function DispatchPage() {
-  return <div>DispatchPage</div>
+  const [selectedRouteId, setSelectedRouteId] = useState<number>(0)
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['route'],
+    queryFn: () => {
+      getRouteList()
+    },
+  })
+
+  if (isLoading) {
+    return <div> is Loading ... </div>
+  }
+  return (
+    <Page>
+      <div className="mx-auto flex h-3/6 w-[1536px] justify-center max-2xl:mx-10 max-2xl:w-full max-2xl:flex-row">
+        <NaverMap />
+        <RouteList routeList={data} setSelectedRouteId={setSelectedRouteId} />
+      </div>
+      <ItemListView selectedRouteId />
+    </Page>
+  )
 }
