940d707447cda0dcd1ccefc234ccf9441a28c765
이수현 <suhyeon9120@gmail.com>
2024-04-25 15:53:32 +0900

    Feat: [BE] StationRepository 추가
    S10O31A405-169

--- backend/src/main/java/kr/co/littleriders/backend/domain/station/service/StationRepository.java
+++ backend/src/main/java/kr/co/littleriders/backend/domain/station/service/StationRepository.java
@@ -2,9 +2,14 @@ package kr.co.littleriders.backend.domain.station.service;
 
 
 import kr.co.littleriders.backend.domain.station.entity.Station;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface StationRepository extends JpaRepository<Station,Long> {
+    Page<Station> findByNameAndAcademyId(String name, Long academyId, Pageable pageable);
+
+    boolean existsByNameAndAcademyId(String name, Long academyId);
 }
