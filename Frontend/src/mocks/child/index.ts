4a3d06c8da7964f2a5a2f65c8a4f9d0ef3c6d3d8
김민준 <alswns1285@gmail.com>
2024-04-25 17:06:49 +0900

    Feat: [FE] 원생 승인 거부Mock 구현
    S10P31A405-142

--- Frontend/src/mocks/child/index.ts
+++ Frontend/src/mocks/child/index.ts
@@ -3,6 +3,10 @@ import { HttpResponse, http } from 'msw'
 
 const BASE_URL = '/api'
 
+type DeleteChildRequestBody = {
+  academyChildAllowPendingList: number[]
+}
+
 export const handlers = [
   http.get(`${BASE_URL}/admin/child/pending`, () => {
     console.log('Captured a "GET /child/pending" request')
@@ -10,17 +14,22 @@ export const handlers = [
       pendingChildList,
     })
   }),
-  // http.delete(`${BASE_URL}/admin/child/pending`, async ({ request }) => {
-  //   console.log('Captured a "DELETE /child/pending" request')
-  //   const { academyChildAllowPendingList } = await request.json()
+  http.delete<never, DeleteChildRequestBody, never>(
+    `${BASE_URL}/admin/child/pending`,
+    async ({ request }) => {
+      console.log('Captured a "DELETE /child/pending" request')
+      const { academyChildAllowPendingList } = await request.json()
 
-  //   for(const id of academyChildAllowPendingList) {
-  //     const index = pendingChildList.findIndex(child => child.academy_child_allow_pending_id === id)
-  //     if(index === -1) {
-  //       pendingChildList.splice(index, 1)
-  //     }
-  //   }
+      for (const id of academyChildAllowPendingList) {
+        const index = pendingChildList.findIndex(
+          (child) => child.academyChildAllowPendingId === id,
+        )
+        if (index !== -1) {
+          pendingChildList.splice(index, 1)
+        }
+      }
 
-  //   return HttpResponse.
-  // }),
+      return new HttpResponse(null, { status: 200 })
+    },
+  ),
 ]
