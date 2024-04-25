fed0dc6368bd29c23696647aed1615e9b2d11b11
김민준 <alswns1285@gmail.com>
2024-04-25 13:36:03 +0900

    Feat: [FE] 원생대기목록 컴포넌트 UI 구현

---
+++ Frontend/src/pages/ChildPage/PendingList/PendingListItem.tsx
@@ -0,0 +1,60 @@
+import Button from '@components/Shared/Button'
+import GenderIcon from '@components/Shared/GenderIcon'
+
+import { formatBirthToAge } from '@utils/formatUtils'
+
+import { PendingChildInfo } from '@types'
+
+interface Props {
+  pendingChildInfo: PendingChildInfo
+  onChildCheck: (id: number, isChecked: boolean) => void
+  isChecked: boolean
+}
+
+export default function PendingListItem({
+  pendingChildInfo,
+  onChildCheck,
+  isChecked,
+}: Props) {
+  const {
+    academyChildAllowPendingId,
+    address,
+    childBirthDate,
+    childName,
+    gender,
+  } = pendingChildInfo
+
+  return (
+    <tr className="border-b bg-white hover:bg-gray-50">
+      <td className="w-4 p-4">
+        <input
+          type="checkbox"
+          className="h-4 w-4 rounded"
+          onChange={(e) =>
+            onChildCheck(academyChildAllowPendingId, e.target.checked)
+          }
+          checked={isChecked}
+        />
+      </td>
+      <td className="px-6 py-4">
+        <span>{childName}</span>
+      </td>
+      <td className="px-6 py-4">
+        <GenderIcon gender={gender} style="w-full" size={20} />
+      </td>
+      <td className="px-6 py-4">{formatBirthToAge(childBirthDate)}</td>
+      <td className="px-6 py-4">{childBirthDate}</td>
+      <td className="px-6 py-4">{address}</td>
+      <td className="px-6 py-4">
+        <div className="flex justify-end gap-3">
+          <Button color="bg-lightgreen" onClick={() => {}}>
+            <span className="font-bold text-white">승인</span>
+          </Button>
+          <Button color="bg-red" onClick={() => {}}>
+            <span className="font-bold text-white">거절</span>
+          </Button>
+        </div>
+      </td>
+    </tr>
+  )
+}
--- Frontend/src/pages/ChildPage/index.tsx
+++ Frontend/src/pages/ChildPage/index.tsx
@@ -1,3 +1,31 @@
+import PendingList from '@pages/ChildPage/PendingList'
+
+import Spacing from '@components/Shared/Spacing'
+
+import { useQuery } from '@tanstack/react-query'
+
+import { getPendingChildList } from '@apis/child'
+
+import Page from '@layouts/Page'
+
 export default function ChildPage() {
-  return <div>ChildPage</div>
+  const { data, isLoading } = useQuery({
+    queryKey: [],
+    queryFn: getPendingChildList,
+    select: (data) => {
+      const pendingChildList = data.data.pendingChildList
+      return pendingChildList
+    },
+  })
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <Page>
+      <Spacing style="h-[60px]" />
+      <PendingList pendingChildList={data} />
+    </Page>
+  )
 }
