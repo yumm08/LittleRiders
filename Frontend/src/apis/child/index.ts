b0d64ef2478e85898eeadf7862d790656b48855a
김민준 <alswns1285@gmail.com>
2024-04-25 17:07:41 +0900

    Feat: [FE] 원생 승인 거절 API 추가
    S10P31A405-142

--- Frontend/src/apis/child/index.ts
+++ Frontend/src/apis/child/index.ts
@@ -2,10 +2,24 @@ import axiosInstance from '@utils/httpCommons'
 
 const BASE_URL = 'admin'
 
+/**
+ * 원생 대기 목록을 불러오는 함수
+ */
 export const getPendingChildList = async () => {
   const response = await axiosInstance.get(`${BASE_URL}/child/pending`)
 
-  console.log(response)
+  return response
+}
+
+/**
+ * 원생 승인 요청을 거절하는 함수
+ */
+export const deletePendingChildList = async (
+  academyChildAllowPendingList: number[],
+) => {
+  const response = await axiosInstance.delete(`${BASE_URL}/child/pending`, {
+    data: { academyChildAllowPendingList },
+  })
 
   return response
 }
