447c9e87690acbf144457b434bd40a248d92c3d7
김민준 <alswns1285@gmail.com>
2024-04-25 11:06:11 +0900

    Feat: [FE] 원생대기목록 type 추가
    S10P31A405-142

---
+++ Frontend/src/types/child.ts
@@ -0,0 +1,8 @@
+export type PendingChildInfo = {
+  academyChildAllowPendingId: number
+  address: string
+  childBirthDate: string
+  childName: string
+  familyPhoneNumber: string
+  gender: 'FEMALE' | 'MALE'
+}
