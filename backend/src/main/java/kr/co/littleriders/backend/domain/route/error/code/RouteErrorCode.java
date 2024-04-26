f19ec202858b60c5600d0a4660c9e4ba5c5837ed
이수현 <suhyeon9120@gmail.com>
2024-04-26 15:05:34 +0900

    Feat: [BE] RouteErrorCode에 002, 003 추가
    S10O31A405-179
    
    DUPLICATE_NAME(002)와 FORBIDDEN(003)을 추가하였습니다.

--- backend/src/main/java/kr/co/littleriders/backend/domain/route/error/code/RouteErrorCode.java
+++ backend/src/main/java/kr/co/littleriders/backend/domain/route/error/code/RouteErrorCode.java
@@ -7,8 +7,9 @@ import org.springframework.http.HttpStatus;
 public enum RouteErrorCode implements LittleRidersErrorCode {
 
 
-    NOT_FOUND(HttpStatus.NOT_FOUND, "001", "경로를 찾을수 없습니다.");
-
+    NOT_FOUND(HttpStatus.NOT_FOUND, "001", "경로를 찾을수 없습니다."),
+    DUPLICATE_NAME(HttpStatus.CONFLICT, "002", "이미 존재하는 경로 이름입니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "003", "해당 데이터에 접근할 권한이 없습니다.");
 
     RouteErrorCode(HttpStatus status, String code, String message) {
         this.status = status;
