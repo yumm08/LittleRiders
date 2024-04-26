321510092a1fde3af6adb563a4c6713539cf8a91
김민준 <alswns1285@gmail.com>
2024-04-26 17:51:46 +0900

    Rename: [FE] 원생 대기 목록 관련 mock 파일 이동

---
+++ Frontend/src/mocks/child/pendingChild.ts
@@ -0,0 +1,51 @@
+import { pendingChildList } from '@mocks/child/dummy'
+import { HttpResponse, http } from 'msw'
+
+type PostPendingChildReqestBody = {
+  academyChildAllowPendingList: number[]
+}
+type DeletePendingChildRequestBody = {
+  academyChildAllowPendingList: number[]
+}
+
+const BASE_URL = '/api/admin/child'
+
+export const handlers = [
+  http.get(`${BASE_URL}/pending`, () => {
+    return HttpResponse.json(pendingChildList)
+  }),
+  http.post<never, PostPendingChildReqestBody, never>(
+    `${BASE_URL}/pending`,
+    async ({ request }) => {
+      const { academyChildAllowPendingList } = await request.json()
+
+      for (const id of academyChildAllowPendingList) {
+        const index = pendingChildList.findIndex(
+          (child) => child.academyChildAllowPendingId === id,
+        )
+        if (index !== -1) {
+          pendingChildList.splice(index, 1)
+        }
+      }
+
+      return new HttpResponse(null, { status: 200 })
+    },
+  ),
+  http.delete<never, DeletePendingChildRequestBody, never>(
+    `${BASE_URL}/pending`,
+    async ({ request }) => {
+      const { academyChildAllowPendingList } = await request.json()
+
+      for (const id of academyChildAllowPendingList) {
+        const index = pendingChildList.findIndex(
+          (child) => child.academyChildAllowPendingId === id,
+        )
+        if (index !== -1) {
+          pendingChildList.splice(index, 1)
+        }
+      }
+
+      return new HttpResponse(null, { status: 200 })
+    },
+  ),
+]
