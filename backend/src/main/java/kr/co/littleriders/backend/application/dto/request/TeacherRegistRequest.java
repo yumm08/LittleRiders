e4278b9ae8108fb5331c887426541b400af5f3f4
Yunji Lee <luj2868@gmail.com>
2024-04-26 17:02:14 +0900

    Fix: [BE] 선탑자 엔티티 매핑 방식 변경
    S10P31A405-152

--- backend/src/main/java/kr/co/littleriders/backend/application/dto/request/TeacherRegistRequest.java
+++ backend/src/main/java/kr/co/littleriders/backend/application/dto/request/TeacherRegistRequest.java
@@ -3,6 +3,9 @@ package kr.co.littleriders.backend.application.dto.request;
 import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.validation.constraints.NotBlank;
+import kr.co.littleriders.backend.domain.academy.entity.Academy;
+import kr.co.littleriders.backend.domain.teacher.entity.Teacher;
+import kr.co.littleriders.backend.domain.teacher.entity.TeacherStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,4 +22,8 @@ public class TeacherRegistRequest {
 	private String phoneNumber;
 
 	private MultipartFile image;
+
+	public Teacher toEntity(Academy academy) {
+		return Teacher.of(this.name, this.phoneNumber, academy, TeacherStatus.WORK);
+	}
 }
