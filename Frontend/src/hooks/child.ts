8498adff697de104538414c5ecf69775ba38715f
김민준 <alswns1285@gmail.com>
2024-04-26 10:54:14 +0900

    Feat: [FE] 원생 가입 요청 관련 hook 구현
    S10P31A405-142

---
+++ Frontend/src/hooks/child.ts
@@ -0,0 +1,48 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+
+import {
+  deletePendingChildList,
+  getPendingChildList,
+  postPendingChildList,
+} from '@apis/child'
+
+export const useFetchPendingChildList = () => {
+  const { data: pendingChildList, ...rest } = useQuery({
+    queryKey: ['getPendingChildList'],
+    queryFn: getPendingChildList,
+    select: (data) => {
+      const pendingChildList = data.data.pendingChildList
+      return pendingChildList
+    },
+  })
+
+  return { pendingChildList, ...rest }
+}
+
+export const useApproveChild = () => {
+  const queryClient = useQueryClient()
+
+  const { mutate: approveChild, ...rest } = useMutation({
+    mutationFn: (checkChildIdList: number[]) =>
+      postPendingChildList(checkChildIdList),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['getPendingChildList'] })
+    },
+  })
+
+  return { approveChild, ...rest }
+}
+
+export const useRejectChild = () => {
+  const queryClient = useQueryClient()
+
+  const { mutate: rejectChild, ...rest } = useMutation({
+    mutationFn: (checkChildIdList: number[]) =>
+      deletePendingChildList(checkChildIdList),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['getPendingChildList'] })
+    },
+  })
+
+  return { rejectChild, ...rest }
+}
