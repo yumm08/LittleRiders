512960c8fb9c8b042e49f363163690c4757bf96a
gwanu-dev <devkwanwoo@gmail.com>
2024-04-26 18:01:08 +0900

    Feat: [FE] Station, Route Get Mock 함수 작성
    S10P31A405-162

--- Frontend/src/mocks/browser.ts
+++ Frontend/src/mocks/browser.ts
@@ -1,9 +1,13 @@
-import { routeHandlers } from './dispatch/routeHandlers'
-
+import { routeHandlers } from '@mocks/dispatch/routeHandlers'
+import { stationHandlers } from '@mocks/dispatch/stationHandlers'
 import { handlers } from '@mocks/handlers'
 import { setupWorker } from 'msw/browser'
 
-export const worker = setupWorker(...handlers, ...routeHandlers)
+export const worker = setupWorker(
+  ...handlers,
+  ...routeHandlers,
+  ...stationHandlers,
+)
 
 if (process.env.NODE_ENV === 'development') {
   worker.start()
