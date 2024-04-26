b2f717a0291d4e64de7207b660fe1ed9513df518
김민준 <alswns1285@gmail.com>
2024-04-26 17:11:17 +0900

    Rename: [FE] 대기중인 원생 타입 위치 변경

---
+++ Frontend/src/types/child/pendingChild.ts
@@ -0,0 +1,10 @@
+import { Gender } from '@types'
+
+export type PendingChildInfo = {
+  academyChildAllowPendingId: number
+  address: string
+  childBirthDate: string
+  childName: string
+  familyPhoneNumber: string
+  gender: Gender
+}
