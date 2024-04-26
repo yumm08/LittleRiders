e9906982727c15c039487554085cfe0ecdcd57d1
이수현 <suhyeon9120@gmail.com>
2024-04-26 15:03:50 +0900

    Feat: [BE] Route 엔티티에 생성자 추가
    S10O31A405-179

--- backend/src/main/java/kr/co/littleriders/backend/domain/route/entity/Route.java
+++ backend/src/main/java/kr/co/littleriders/backend/domain/route/entity/Route.java
@@ -3,9 +3,13 @@ package kr.co.littleriders.backend.domain.route.entity;
 import jakarta.persistence.*;
 import kr.co.littleriders.backend.domain.academy.entity.Academy;
 import kr.co.littleriders.backend.domain.routeinfo.entity.RouteStation;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@NoArgsConstructor
+@Getter
 @Entity
 @Table(name = "route")
 public class Route {
@@ -25,5 +29,19 @@ public class Route {
 
     @OneToMany(mappedBy = "route")
     private List<RouteStation> routeStationList; // 정류장 목록
+
+    private Route(final Academy academy, String name, List<RouteStation> routeStationList) {
+        this.academy = academy;
+        this.name = name;
+        this.routeStationList = routeStationList;
+    }
+
+    public static Route of(final Academy academy, String name, List<RouteStation> routeStationList) {
+        return new Route(
+                academy,
+                name,
+                routeStationList
+        );
+    }
 
 }
