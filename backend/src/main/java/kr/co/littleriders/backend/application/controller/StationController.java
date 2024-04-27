311c100b15b4fe58e5875c85f169b6bf566cf5a8
이수현 <suhyeon9120@gmail.com>
2024-04-28 08:10:23 +0900

    Ref: [BE] ResponseEntity 타입 수정
    S10P31A405-169

--- backend/src/main/java/kr/co/littleriders/backend/application/controller/StationController.java
+++ backend/src/main/java/kr/co/littleriders/backend/application/controller/StationController.java
@@ -1,11 +1,14 @@
 package kr.co.littleriders.backend.application.controller;
 
 import kr.co.littleriders.backend.application.dto.request.StationCreateRequest;
+import kr.co.littleriders.backend.application.dto.response.StationResponse;
 import kr.co.littleriders.backend.application.facade.StationFacade;
 import kr.co.littleriders.backend.domain.academy.entity.Academy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/station")
@@ -15,7 +18,7 @@ public class StationController {
 
     // 정류장 등록
     @PostMapping
-    public ResponseEntity<?> createStation(@RequestBody StationCreateRequest createRequest) {
+    public ResponseEntity<Void> createStation(@RequestBody StationCreateRequest createRequest) {
         Academy academy = null;
         stationFacade.createStation(academy, createRequest);
         return ResponseEntity.ok().build();
@@ -23,7 +26,7 @@ public class StationController {
 
     // 정류장 목록 조회
     @GetMapping
-    public ResponseEntity<?> searchStationByName(@RequestParam(required = false, defaultValue = "") String name, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+    public ResponseEntity<List<StationResponse>> searchStationByName(@RequestParam(required = false, defaultValue = "") String name, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
         Academy academy = null;
         return ResponseEntity.ok().body(stationFacade.searchByName(name, academy));
     }
