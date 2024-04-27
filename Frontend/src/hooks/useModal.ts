6885f2d674ff1dd5859387dadd0efbb179bf58f7
박희준 <wooryjoon@gmail.com>
2024-04-27 16:52:23 +0900

    Feat: [FE] useModal TSdoc 추가

--- Frontend/src/hooks/useModal.ts
+++ Frontend/src/hooks/useModal.ts
@@ -1,5 +1,8 @@
 import { useEffect } from 'react'
 
+/**
+ * @summary 모달창 뒷 부분 스크롤을 막기 위한 훅입니다.
+ */
 export default function useModal() {
   useEffect(() => {
     document.documentElement.style.scrollbarGutter = 'stable' // 추가
