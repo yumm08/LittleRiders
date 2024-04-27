82e2fe5365114322154c98f03231e162218b2aab
박희준 <wooryjoon@gmail.com>
2024-04-27 16:51:30 +0900

    Ref: [FE] 모달 컴포넌트 변수명 수정

--- Frontend/src/components/Shared/Modal.tsx
+++ Frontend/src/components/Shared/Modal.tsx
@@ -6,28 +6,28 @@ import BackDrop from './BackDrop'
 import ModalPortal from './ModalPortal'
 
 interface Props {
-  openModal: MouseEventHandler<HTMLDivElement>
+  modalSwitch: MouseEventHandler<HTMLDivElement>
   children: React.ReactNode
   modalTitle: string
 }
 /**
  *
- * @param openModal 모달의 on/off 상태를 관리할 함수를 넘긴다
+ * @param modalSwitch 모달의 on/off 상태를 관리할 함수를 넘긴다
  * @param modalTitle 모달의 제목
  * @returns
  */
-export default function Modal({ openModal, children, modalTitle }: Props) {
+export default function Modal({ modalSwitch, children, modalTitle }: Props) {
   useModal()
 
   return (
     <ModalPortal>
       {/* 뒷 배경  */}
-      <BackDrop onClick={openModal}>
+      <BackDrop onClick={modalSwitch}>
         <div
           onClick={(e) => {
             e.stopPropagation()
           }}
-          className=" relative left-1/2 top-1/2 z-20 w-[760px] -translate-x-1/2 -translate-y-1/2 flex-col rounded-md bg-white p-4 text-center"
+          className=" relative left-1/2 top-1/2 z-20 flex w-[760px] -translate-x-1/2 -translate-y-1/2 flex-col rounded-md bg-white p-4 text-center"
         >
           <strong className="mx-auto inline-block">{modalTitle}</strong>
           {children}
