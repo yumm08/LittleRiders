f4878b419dfb7e62b23c87b7a7d9737521a3b976
박희준 <wooryjoon@gmail.com>
2024-04-27 16:51:13 +0900

    Ref: [FE] 버튼 컴포넌트 TSDoc 수정

--- Frontend/src/components/Shared/Button.tsx
+++ Frontend/src/components/Shared/Button.tsx
@@ -6,7 +6,6 @@ interface Props {
 }
 /**
  * @summary 버튼 컴포넌트
- * @param innerText : 버튼 내부 텍스트
  * @param full(optional) : 레이아웃 전체 꽉 채우고 싶을 때 사용
  * @param onClick : 버튼 클릭 시 실행할 콜백함수
  * @param color : 버튼 배경 색깔  'bg-' 형태로 주입
