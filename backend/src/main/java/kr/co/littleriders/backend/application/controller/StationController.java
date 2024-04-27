326c5dd75a53eca5a9ab7d9ce266594f5f788adb
이수현 <suhyeon9120@gmail.com>
2024-04-28 08:24:54 +0900

    Ref: [BE] 파라미터에 AuthAcademy 추가
    S10P31A405-169

--- backend/src/main/java/kr/co/littleriders/backend/application/controller/StationController.java
+++ backend/src/main/java/kr/co/littleriders/backend/application/controller/StationController.java
@@ -3,7 +3,8 @@ package kr.co.littleriders.backend.application.controller;
 import kr.co.littleriders.backend.application.dto.request.StationCreateRequest;
 import kr.co.littleriders.backend.application.dto.response.StationResponse;
 import kr.co.littleriders.backend.application.facade.StationFacade;
-import kr.co.littleriders.backend.domain.academy.entity.Academy;
+import kr.co.littleriders.backend.global.auth.annotation.Auth;
+import kr.co.littleriders.backend.global.auth.dto.AuthAcademy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,16 +19,14 @@ public class StationController {
 
     // 정류장 등록
     @PostMapping
-    public ResponseEntity<Void> createStation(@RequestBody StationCreateRequest createRequest) {
-        Academy academy = null;
-        stationFacade.createStation(academy, createRequest);
+    public ResponseEntity<Void> createStation(@Auth AuthAcademy authAcademy, @RequestBody StationCreateRequest createRequest) {
+        stationFacade.createStation(authAcademy, createRequest);
         return ResponseEntity.ok().build();
     }
 
     // 정류장 목록 조회
     @GetMapping
-    public ResponseEntity<List<StationResponse>> searchStationByName(@RequestParam(required = false, defaultValue = "") String name, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
-        Academy academy = null;
-        return ResponseEntity.ok().body(stationFacade.searchByName(name, academy));
+    public ResponseEntity<List<StationResponse>> searchStationByName(@Auth AuthAcademy authAcademy, @RequestParam(required = false, defaultValue = "") String name, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+        return ResponseEntity.ok().body(stationFacade.searchByName(name, authAcademy));
     }
 }
