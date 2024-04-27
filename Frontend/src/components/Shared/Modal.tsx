0eea18baf085eb331bb1e9d914fde5bf2260585c
박희준 <wooryjoon@gmail.com>
2024-04-27 15:45:55 +0900

    Feat: [FE] Modal컴포넌트 이벤트 버블링 방지

--- Frontend/src/components/Shared/Modal.tsx
+++ Frontend/src/components/Shared/Modal.tsx
@@ -10,13 +10,25 @@ interface Props {
   children: React.ReactNode
   modalTitle: string
 }
+/**
+ *
+ * @param openModal 모달의 on/off 상태를 관리할 함수를 넘긴다
+ * @param modalTitle 모달의 제목
+ * @returns
+ */
 export default function Modal({ openModal, children, modalTitle }: Props) {
   useModal()
+
   return (
     <ModalPortal>
       {/* 뒷 배경  */}
       <BackDrop onClick={openModal}>
-        <div className=" relative left-1/2 top-1/2 z-20 w-[760px] -translate-x-1/2 -translate-y-1/2 flex-col rounded-md bg-white p-4 text-center">
+        <div
+          onClick={(e) => {
+            e.stopPropagation()
+          }}
+          className=" relative left-1/2 top-1/2 z-20 w-[760px] -translate-x-1/2 -translate-y-1/2 flex-col rounded-md bg-white p-4 text-center"
+        >
           <strong className="mx-auto inline-block">{modalTitle}</strong>
           {children}
         </div>
