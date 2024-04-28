c3c3782c00aca0f25f60bb6d374eab444cc4f5e6
wooryjoon <wooryjoon@gmail.com>
2024-04-28 21:56:40 +0900

    Feat: [FE] 파일업로드 훅 생성

--- Frontend/src/hooks/useFileUpload.ts
+++ Frontend/src/hooks/useFileUpload.ts
@@ -1,14 +1,19 @@
 import React, { useState } from 'react'
 
 export default function useFileUpload() {
+  // Url로 변환한 이미지 파일
   const [uploadImgUrl, setUploadImgUrl] = useState<string>('')
+  //   이미지 파일
+  const [uploadFile, setUploadFile] = useState<File | null>(null)
+
   const onChangeImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const uploadFile = (e.target.files as FileList)[0]
+    const currentFile = (e.target.files as FileList)[0]
+    setUploadFile(currentFile)
     const reader = new FileReader()
-    reader.readAsDataURL(uploadFile)
+    reader.readAsDataURL(currentFile)
     reader.onloadend = () => {
       setUploadImgUrl(reader.result as string)
     }
   }
-  return { uploadImgUrl, onChangeImageUpload }
+  return { uploadImgUrl, onChangeImageUpload, uploadFile }
 }
