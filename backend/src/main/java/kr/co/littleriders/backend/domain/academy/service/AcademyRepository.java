6f04c7329aea9812bed94bbcf76111dbc971ee9c
yunjiL <luj2868@gmail.com>
2024-04-28 18:44:53 +0900

    Fix: [BE] 레포지토리 param 어노테이션 추가
    S10P31A405-184

--- backend/src/main/java/kr/co/littleriders/backend/domain/academy/service/AcademyRepository.java
+++ backend/src/main/java/kr/co/littleriders/backend/domain/academy/service/AcademyRepository.java
@@ -5,16 +5,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-interface AcademyRepository extends JpaRepository<Academy, Long> {
+interface AcademyRepository extends JpaRepository<Academy, Long>, AcademyCustomRepository {
     Optional<Academy> findByEmail(String email);
 
     boolean existsByEmail(String email);
 
-    @Query("SELECT a FROM Academy a WHERE a.name LIKE %:name%")
-    Page<Academy> findByName(String name, Pageable pageable);
+    @Query("SELECT a FROM Academy a WHERE a.name LIKE concat('%', :name, '%') ")
+    Page<Academy> findByName(@Param("name") String name, Pageable pageable);
 }
