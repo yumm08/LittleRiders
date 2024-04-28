0be72e8f130b95d3bb6ecd8e6f485d36cb46dd87
이수현 <suhyeon9120@gmail.com>
2024-04-29 08:32:46 +0900

    Ref: [BE] routeStationList 반환형 수정
    S10P31A405-179
    
    List<RouteStation> -> List<RouteStationResponse> 로 수정하였습니다.

--- backend/src/main/java/kr/co/littleriders/backend/application/dto/response/RouteResponse.java
+++ backend/src/main/java/kr/co/littleriders/backend/application/dto/response/RouteResponse.java
@@ -1,24 +1,26 @@
 package kr.co.littleriders.backend.application.dto.response;
 
 import kr.co.littleriders.backend.domain.route.entity.Route;
-import kr.co.littleriders.backend.domain.routeinfo.entity.RouteStation;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Getter
 public class RouteResponse {
     private long id;
     private String name;
-    private List<RouteStation> stationList;
+    private List<RouteStationResponse> routeStationList;
 
     public static RouteResponse from(Route route) {
         long id = route.getId();
         String name = route.getName();
-        List<RouteStation> stationList = route.getRouteStationList();
+        List<RouteStationResponse> routeStationList = route.getRouteStationList().stream()
+                .map(RouteStationResponse::from)
+                .collect(Collectors.toList());
 
-        return new RouteResponse(id, name, stationList);
+        return new RouteResponse(id, name, routeStationList);
     }
 }
