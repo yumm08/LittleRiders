33328ff81c80b235ad0b119c41c3fa6caadd0e8f
Yunji Lee <luj2868@gmail.com>
2024-04-29 03:58:19 +0900

    Feat: [BE] 학원 원생 조회 기능 구현
    S10P31A405-188

--- backend/src/main/java/kr/co/littleriders/backend/domain/history/service/ChildHistoryServiceImpl.java
+++ backend/src/main/java/kr/co/littleriders/backend/domain/history/service/ChildHistoryServiceImpl.java
@@ -1,5 +1,7 @@
 package kr.co.littleriders.backend.domain.history.service;
 
+import kr.co.littleriders.backend.domain.academy.entity.AcademyChild;
+import kr.co.littleriders.backend.domain.history.entity.ChildHistory;
 import org.springframework.stereotype.Service;
 
 import kr.co.littleriders.backend.domain.history.ChildHistoryService;
@@ -8,4 +10,11 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 class ChildHistoryServiceImpl implements ChildHistoryService {
+
+    private final ChildHistoryRepository childHistoryRepository;
+
+    @Override
+    public ChildHistory findByCreatedAt(AcademyChild academyChild) {
+        return childHistoryRepository.findByCreatedAt(academyChild.getChild(), academyChild.getUpdatedAt());
+    }
 }
