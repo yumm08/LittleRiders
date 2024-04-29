a2ecf97535cd19607db22a73b6bd863fc5648ee5
김민준 <alswns1285@gmail.com>
2024-04-29 13:19:46 +0900

    Feat: [FE] 로그인 성공시 상태 변경
    S10P31A405-149
    
    - 로그인 성공시 로그인 상태 true로 변경

--- Frontend/src/hooks/auth/signIn.ts
+++ Frontend/src/hooks/auth/signIn.ts
@@ -1,3 +1,5 @@
+import { useAuthStore } from '@stores/authStore'
+
 import { useMutation } from '@tanstack/react-query'
 
 import { postSignIn } from '@apis/auth'
@@ -10,12 +12,16 @@ import Swal from 'sweetalert2'
 export const useSignIn = () => {
   const navigate = useNavigate()
 
+  const { setSignedIn } = useAuthStore()
+
   const { mutate: signIn, ...rest } = useMutation({
     mutationFn: (signInInfo: SignInInfo) => postSignIn(signInInfo),
     onSuccess: async (response) => {
       const { status } = response
 
       if (status === HttpStatusCode.Ok) {
+        setSignedIn()
+
         const result = await Swal.fire({
           text: '로그인에 성공했습니다.',
           icon: 'success',
