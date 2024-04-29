d37b23ada9f451ec05ccc34e19638043a67eb914
김민준 <alswns1285@gmail.com>
2024-04-29 15:45:23 +0900

    Fix: [FE] 빌드 에러 해결
    - sweetAlert2 버전 문제

--- Frontend/src/components/Route/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ItemListView } from '@components/Route/ItemListView'
-export { default as NaverMap } from '@components/Route/NaverMap'
-export { default as RouteList } from '@components/Route/RouteList'
