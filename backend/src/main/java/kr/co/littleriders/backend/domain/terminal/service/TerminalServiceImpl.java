00ef73b65a0875cec56e6b706ff2e249474fe646
김도현 <nanayagoon@daum.net>
2024-04-30 07:20:21 +0900

    Fix: [BE] implements 안된 클래스 수정
    S10P31A405-195

--- backend/src/main/java/kr/co/littleriders/backend/domain/terminal/service/TerminalServiceImpl.java
+++ backend/src/main/java/kr/co/littleriders/backend/domain/terminal/service/TerminalServiceImpl.java
@@ -1,11 +1,12 @@
 package kr.co.littleriders.backend.domain.terminal.service;
 
+import kr.co.littleriders.backend.domain.terminal.TerminalService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
-class TerminalServiceImpl {
+class TerminalServiceImpl implements TerminalService {
     private final TerminalRepository terminalRepository;
 }
