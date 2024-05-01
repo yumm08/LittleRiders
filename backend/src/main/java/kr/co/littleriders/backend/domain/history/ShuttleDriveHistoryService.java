fe643a3b91992bb4661531ae286d148dade035c7
김도현 <nanayagoon@daum.net>
2024-05-02 00:00:18 +0900

    Feat: [BE] 운행 기록의 shuttleId유니크 날짜 조회 및 날짜로 목록 조회
    S10P31A405-251
    
    mongodb 의 Aggregation 기반 조회를 구현 했습니다

--- backend/src/main/java/kr/co/littleriders/backend/domain/history/ShuttleDriveHistoryService.java
+++ backend/src/main/java/kr/co/littleriders/backend/domain/history/ShuttleDriveHistoryService.java
@@ -2,8 +2,16 @@ package kr.co.littleriders.backend.domain.history;
 
 import kr.co.littleriders.backend.domain.history.entity.ShuttleDriveHistory;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ShuttleDriveHistoryService {
 
     ShuttleDriveHistory findById(String id);
     String save(ShuttleDriveHistory shuttleDriveHistory);
+
+    List<ShuttleDriveHistory> findByShuttleIdAndStartAt(long shuttleId, int year, int month, int day);
+
+
+    List<LocalDateTime> findDistinctYearAndMonthAndDayListByShuttleId(long shuttleId);
 }
