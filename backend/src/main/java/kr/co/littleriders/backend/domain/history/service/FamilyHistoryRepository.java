7a29816b5d0a3d30d0d262ac1a2aa8c9d4905e89
김도현 <nanayagoon@daum.net>
2024-05-03 21:26:00 +0900

    Feat: [BE] 회원가입시 회원 이력 추가
    S10P31A405-260

--- backend/src/main/java/kr/co/littleriders/backend/domain/history/service/FamilyHistoryRepository.java
+++ backend/src/main/java/kr/co/littleriders/backend/domain/history/service/FamilyHistoryRepository.java
@@ -1,15 +1,15 @@
 package kr.co.littleriders.backend.domain.history.service;
 
-import java.time.LocalDateTime;
-
+import kr.co.littleriders.backend.domain.family.entity.Family;
+import kr.co.littleriders.backend.domain.history.entity.FamilyHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import kr.co.littleriders.backend.domain.family.entity.Family;
-import kr.co.littleriders.backend.domain.history.entity.FamilyHistory;
+import java.time.LocalDateTime;
 
 @Repository
 interface FamilyHistoryRepository extends JpaRepository<FamilyHistory, Long> {
 
 	FamilyHistory findByFamilyAndCreatedAtBeforeOrderByCreatedAtDesc(Family family, LocalDateTime updatedAt);
+	
 }
