35142fe86d1aff1f2f13710933dc221e317317a2
김도현 <nanayagoon@daum.net>
2024-05-03 21:01:26 +0900

    Fix: [BE] 컨플릭트 해결
    S10P31A405-251

--- backend/src/main/java/kr/co/littleriders/backend/domain/shuttle/entity/ShuttleLocationHistory.java
+++ backend/src/main/java/kr/co/littleriders/backend/domain/shuttle/entity/ShuttleLocationHistory.java
@@ -37,6 +37,7 @@ public class ShuttleLocationHistory {
     }
 
     @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
     public static class LocationInfo {
         private double latitude;
 
