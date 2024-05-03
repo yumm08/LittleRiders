a63563dc9b55d2c605e1a5196c0dc02179514c2e
이수현 <suhyeon9120@gmail.com>
2024-05-03 14:48:55 +0900

    Test: [BE] 운행 가능 노선 목록 조회 테스트
    S10O31A405-191

---
+++ backend/src/test/java/kr/co/littleriders/backend/application/facade/ShuttleFacadeTest.java
@@ -0,0 +1,103 @@
+package kr.co.littleriders.backend.application.facade;
+
+import kr.co.littleriders.backend.application.dto.request.ShuttleChildRideRequest;
+import kr.co.littleriders.backend.application.dto.request.ShuttleLocationRequest;
+import kr.co.littleriders.backend.application.dto.request.ShuttleStartRequest;
+import kr.co.littleriders.backend.domain.academy.AcademyChildService;
+import kr.co.littleriders.backend.domain.academy.AcademyFamilyService;
+import kr.co.littleriders.backend.domain.academy.AcademyService;
+import kr.co.littleriders.backend.domain.academy.entity.*;
+import kr.co.littleriders.backend.domain.child.ChildService;
+import kr.co.littleriders.backend.domain.child.entity.Child;
+import kr.co.littleriders.backend.domain.driver.DriverService;
+import kr.co.littleriders.backend.domain.driver.entity.Driver;
+import kr.co.littleriders.backend.domain.driver.entity.DriverStatus;
+import kr.co.littleriders.backend.domain.family.FamilyService;
+import kr.co.littleriders.backend.domain.family.entity.Family;
+import kr.co.littleriders.backend.domain.route.RouteService;
+import kr.co.littleriders.backend.domain.route.entity.Route;
+import kr.co.littleriders.backend.domain.shuttle.ShuttleService;
+import kr.co.littleriders.backend.domain.shuttle.entity.Shuttle;
+import kr.co.littleriders.backend.domain.shuttle.entity.ShuttleStatus;
+import kr.co.littleriders.backend.domain.teacher.TeacherService;
+import kr.co.littleriders.backend.domain.teacher.entity.Teacher;
+import kr.co.littleriders.backend.domain.teacher.entity.TeacherStatus;
+import kr.co.littleriders.backend.domain.terminal.TerminalService;
+import kr.co.littleriders.backend.domain.terminal.entity.Terminal;
+import kr.co.littleriders.backend.global.auth.dto.AuthTerminal;
+import kr.co.littleriders.backend.global.entity.Gender;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+@Transactional
+@SpringBootTest
+public class ShuttleFacadeTest {
+
+    @Autowired
+    private ShuttleFacade shuttleFacade;
+
+    @Autowired
+    private AcademyService academyService;
+
+    @Autowired
+    private TerminalService terminalService;
+
+    @Autowired
+    private ShuttleService shuttleService;
+
+    @Autowired
+    private RouteService routeService;
+
+    @Autowired
+    private DriverService driverService;
+
+    @Autowired
+    private TeacherService teacherService;
+
+    @Autowired
+    private FamilyService familyService;
+
+    @Autowired
+    private ChildService childService;
+
+    @Autowired
+    private AcademyFamilyService academyFamilyService;
+
+    @Autowired
+    private AcademyChildService academyChildService;
+
+    private Academy academy;
+
+    private AuthTerminal authTerminal;
+
+    @BeforeEach
+    void setUP() {
+        academy = Academy.of("a123@gmail.com", "1234", "어린이집B", "서울시 강남구", "010-1111-1111", 35.6, 23.8);
+        academyService.save(academy);
+        Terminal terminal = Terminal.of(academy, "bbbb");
+        terminalService.save(terminal);
+        Shuttle shuttle = Shuttle.of("license1234", "2호차", "a", academy, ShuttleStatus.USE);
+        shuttleService.save(shuttle);
+        authTerminal = AuthTerminal.of(terminal, shuttle);
+    }
+
+    @Nested
+    @DisplayName("운행 가능 노선 목록 조회")
+    class getRouteList {
+
+        @Test
+        @DisplayName("성공")
+        void whenSuccess() throws Exception {
+            shuttleFacade.getRouteList(authTerminal);
+        }
+    }
+
+
+}
