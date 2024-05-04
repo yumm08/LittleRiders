a7f09816d7d870f473250799170894c21410ca07
김도현 <nanayagoon@daum.net>
2024-05-04 15:04:50 +0900

    Ref: [DEVICE] 스레드 옵저버 패턴 리팩토링
    S10P31A405-255

--- device/run.py
+++ device/run.py
@@ -2,14 +2,12 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtCore import QUrl
-import os
+from PyQt5.QtCore import QUrl,QThread, pyqtSignal
 from Model import *
 from APIFetch import APIFetcher
 from Repository import *
-from SensorParser import SensorMockingGenerator
-
-import threading
+from SensorHelper import SensorHelper
+import sys
 
 form_class = uic.loadUiType("untitled.ui")[0]
 modelHelper = ModelHelper()
@@ -18,27 +16,56 @@ positionRepository = PositionRepository(modelHelper=modelHelper)
 terminalNumber = terminalRepository.findById(1).getTerminalNumber()
 apiFetcher = APIFetcher(terminalNumber)
 
-class Observer:
+
+class PositionObserverInterface():
     def __init__(self,function):
         self.function = function
-    def update(self,object):
-        self.function(object)
+    def notify(self,*args,**kwargs):
+        pass
+
+
+class PositionProvider:
+
+    def __init__(self):
+        self.__observers = []
+
+    def register(self,observer:PositionObserverInterface):
+        self.__observers.append(observer)
+
+    def notifyAll(self,*args,**kwargs):
+        for observer in self.__observers:
+            observer.notify(*args,**kwargs)
+
+
+
+class PositionThread(QThread,PositionProvider):
+    positionUploadEvent = pyqtSignal(RMCPosition)
+    def run(self):
+        sensorReceiver = SensorHelper().getSensorReceiver()
+        while True:
+            try:
+                position = sensorReceiver.getPosition()
+                self.notifyAll(position=position)
+            except:
+                pass
+
+    def stop(self):
+        self.terminate()
 
 
 
     
-class MainWindow(QMainWindow, form_class):
+class MainWindow(QMainWindow, form_class,PositionObserverInterface):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.observer = Observer(lambda x:self.courseInfoUpdateByObserver(x))
-        self.exitButton.clicked.connect(lambda x: os.exit())
+        self.exitButton.clicked.connect(self.close)
         self.webview = QWebEngineView()
         self.webview.setUrl(QUrl("https://device.littleriders.co.kr"))
         self.mapLayout.addWidget(self.webview)
         self.webview.loadFinished.connect(self.on_load_finished)
-
         self.mapLoad = False
+
 
     def on_load_finished(self, success):
         if success:
@@ -52,7 +79,15 @@ class MainWindow(QMainWindow, form_class):
         self.longitudeText.setText("경도값이에요")
         self.speedText.setText("속도값이에요")
 
-    def courseInfoUpdateByObserver(self,position):
+
+        #routeList = apiFetcher.getRouteList()
+        #self.uuidText.setText(json.dumps(routeList,ensure_ascii=False,indent=4))
+
+    def notify(self,*args,**kwargs):
+        position = kwargs.get("position",None)
+        if(not position):
+            return
+        position = kwargs["position"]
         latitude = position.getLatitude()
         longitude = position.getLongitude()
         speed = position.getSpeed()
@@ -62,32 +97,15 @@ class MainWindow(QMainWindow, form_class):
         self.longitudeText.setText(f"{longitude}")
         self.speedText.setText(f"{speed}")
 
-        pass
-        #routeList = apiFetcher.getRouteList()
-        #self.uuidText.setText(json.dumps(routeList,ensure_ascii=False,indent=4))
-
-
-
-
-
 
 
 
 app = QApplication(sys.argv)
 win = MainWindow()
 
-observer = win.observer
 
-import time
-def threadingFunction(observer):
-    sensorGenerator = SensorMockingGenerator()
-    while True:
-        position = sensorGenerator.generateSensorValue()
-        observer.update(position)
-        time.sleep(0.1)
-
-thead = threading.Thread(target= lambda :threadingFunction(observer))
-thead.start()
-
+postionThread = PositionThread()
+postionThread.start()
+postionThread.register(win)
 win.show()
 app.exec()
