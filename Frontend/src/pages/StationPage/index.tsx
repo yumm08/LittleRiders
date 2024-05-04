b5941076553236d4779dcfd070272d99153d1e7e
gwanu-dev <devkwanwoo@gmail.com>
2024-05-04 21:21:32 +0900

    Feat: [FE] 정류장 페이지 기본 레이아웃 작성 및 UI 작성
    S10P31A405-214

--- Frontend/src/pages/StationPage/index.tsx
+++ Frontend/src/pages/StationPage/index.tsx
@@ -1,17 +1,25 @@
 import { useRef } from 'react'
 
+import StationList from '@pages/StationPage/StationList'
+
+import MapOverlay from './MapOverlay'
+
 import Page from '@layouts/Page'
 
 export default function StationPage() {
   const mapDiv = useRef<HTMLDivElement>(null)
-
   return (
     <Page>
-      <div className="flex w-full">
-        <div className="w-9/12" ref={mapDiv}>
-          1234356
+      <div className="flex h-[80vh] w-full">
+        <div className="flex h-full w-9/12 justify-center" ref={mapDiv}>
+          <img
+            className="absolute z-10 self-center"
+            src="/src/assets/image/map-center-icon.svg"
+            width={40}
+          />
+          <MapOverlay />
         </div>
-        <div className="w-3/12">asdfasdf</div>
+        <StationList mapDiv={mapDiv} />
       </div>
     </Page>
   )
