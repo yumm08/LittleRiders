da73a47eb845e15d7460f46790b0b5cede88baf4
김도현 <nanayagoon@daum.net>
2024-05-04 15:06:26 +0900

    Ref: [DEVICE] 싱글톤 객체로 변경
    S10P31A405-255

--- device/APIFetch.py
+++ device/APIFetch.py
@@ -1,19 +1,21 @@
 import requests
+from Model import RMCPosition
 class APIFetcher:
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, "_instance"):         # Foo 클래스 객체에 _instance 속성이 없다면
             cls._instance = super().__new__(cls)  # Foo 클래스의 객체를 생성하고 Foo._instance로 바인딩
         return cls._instance                      # Foo._instance를 리턴
 
-    def __init__(self, terminalNumber):
+    def __init__(self, terminalNumber : str):
         cls = type(self)
         if not hasattr(cls, "_init"):             # Foo 클래스 객체에 _init 속성이 없다면
             self.terminalNumber = terminalNumber
             #requests.post(f"{self.BASE_URL}")
+            print("init call apiFetcher")
             cls._init = True
 
 
-    def getRouteList(self):
+    def getRouteList(self) -> dict:
         
         return [
             {"id": 1, "name": "탕수육 코스"},
@@ -22,11 +24,8 @@ class APIFetcher:
             {"id": 4, "name": "소고기 오마카세"}
         ]
 
-
-        
-
-    
-
+    def uploadPosition(self,posistion : RMCPosition) -> None:
+        print(posistion)
 
 if __name__ == "__main__":
 
