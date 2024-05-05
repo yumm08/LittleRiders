56f0fea787142c46295fbad46f71468394f345e3
이수현 <suhyeon9120@gmail.com>
2024-05-05 17:15:38 +0900

    Feat: [BE] 정류장 에러 코드 추가
    S10O31A405-234
    
    003 에러 코드를 추가하였습니다.

--- backend/src/main/java/kr/co/littleriders/backend/domain/station/error/code/StationErrorCode.java
+++ backend/src/main/java/kr/co/littleriders/backend/domain/station/error/code/StationErrorCode.java
@@ -8,7 +8,8 @@ public enum StationErrorCode implements LittleRidersErrorCode {
 
 
     NOT_FOUND(HttpStatus.NOT_FOUND, "001", "정류장을 찾을 수 없습니다."),
-    DUPLICATE_NAME(HttpStatus.CONFLICT, "002", "이미 존재하는 정류장 이름입니다.");
+    DUPLICATE_NAME(HttpStatus.CONFLICT, "002", "이미 존재하는 정류장 이름입니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "003", "해당 작업을 수행할 권한이 없습니다.");
 
     StationErrorCode(HttpStatus status, String code, String message) {
         this.status = status;
