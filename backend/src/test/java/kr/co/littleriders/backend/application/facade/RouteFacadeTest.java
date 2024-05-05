e9b1e35c9671230fb33560eab90816e7774884a3
이수현 <suhyeon9120@gmail.com>
2024-05-05 15:42:33 +0900

    Test: [BE] 노선별 정류장 추가 테스트
    S10O31A405-232

--- backend/src/test/java/kr/co/littleriders/backend/application/facade/RouteFacadeTest.java
+++ backend/src/test/java/kr/co/littleriders/backend/application/facade/RouteFacadeTest.java
@@ -1,9 +1,14 @@
 package kr.co.littleriders.backend.application.facade;
 
 import kr.co.littleriders.backend.application.dto.request.RouteRequest;
+import kr.co.littleriders.backend.application.dto.request.RouteStationRequest;
 import kr.co.littleriders.backend.domain.academy.AcademyService;
 import kr.co.littleriders.backend.domain.academy.entity.*;
 
+import kr.co.littleriders.backend.domain.route.RouteService;
+import kr.co.littleriders.backend.domain.route.entity.Route;
+import kr.co.littleriders.backend.domain.station.StationService;
+import kr.co.littleriders.backend.domain.station.entity.Station;
 import kr.co.littleriders.backend.global.auth.dto.AuthAcademy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,7 +16,12 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Transactional
 @SpringBootTest
 public class RouteFacadeTest {
     @Autowired
@@ -21,6 +31,12 @@ public class RouteFacadeTest {
     private AcademyService academyService;
     private Academy academy;
     private AuthAcademy authAcademy;
+
+    @Autowired
+    private RouteService routeService;
+
+    @Autowired
+    private StationService stationService;
 
     @BeforeEach
     void setUp() {
@@ -40,4 +56,36 @@ public class RouteFacadeTest {
             routeFacade.createRoute(academy.getId(), routeRequest);
         }
     }
+
+    @Nested
+    @DisplayName("노선별 정류장 추가 테스트")
+    class addRouteStation {
+
+        @Test
+        @DisplayName("성공")
+        void whenSuccess() throws Exception {
+            long academyId = academy.getId();
+
+            Route route = Route.of(academy, "등원A", "board");
+            long routeId = routeService.save(route);
+
+            Station station1 = Station.of(academy, "정류장A", 13.4, 13.5);
+            long stationId1 = stationService.save(station1);
+            Station station2 = Station.of(academy, "정류장B", 23.4, 23.5);
+            long stationId2 = stationService.save(station2);
+            Station station3 = Station.of(academy, "정류장C", 33.4, 33.5);
+            long stationId3 = stationService.save(station3);
+
+            List<RouteStationRequest> requestList = new ArrayList<>();
+            RouteStationRequest request1 = new RouteStationRequest(stationId2, 3);
+            RouteStationRequest request2 = new RouteStationRequest(stationId1, 1);
+            RouteStationRequest request3 = new RouteStationRequest(stationId3, 2);
+            requestList.add(request1);
+            requestList.add(request2);
+            requestList.add(request3);
+
+            routeFacade.addRouteStation(academyId, routeId, requestList);
+        }
+    }
+
 }
