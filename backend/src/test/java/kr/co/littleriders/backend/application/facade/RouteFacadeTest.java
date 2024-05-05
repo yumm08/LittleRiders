8662b6376267bc7ac79225f3d7d10a8cb5357e1d
이수현 <suhyeon9120@gmail.com>
2024-05-05 18:56:23 +0900

    Test: [BE] 테스트명 수정 및 함수명 수정
    S10O31A405-235

--- backend/src/test/java/kr/co/littleriders/backend/application/facade/RouteFacadeTest.java
+++ backend/src/test/java/kr/co/littleriders/backend/application/facade/RouteFacadeTest.java
@@ -3,6 +3,7 @@ package kr.co.littleriders.backend.application.facade;
 import kr.co.littleriders.backend.application.dto.request.RouteRequest;
 import kr.co.littleriders.backend.domain.academy.AcademyService;
 import kr.co.littleriders.backend.domain.academy.entity.Academy;
+import kr.co.littleriders.backend.domain.route.RouteService;
 import kr.co.littleriders.backend.global.auth.dto.AuthAcademy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -30,16 +31,15 @@ public class RouteFacadeTest {
     }
 
     @Nested
-    @DisplayName("경로 등록 테스트")
-    class createRoute {
+    @DisplayName("노선 등록 테스트")
+    class createRouteTest {
 
         @Test
         @DisplayName("성공")
         void whenSuccess() throws Exception {
-            RouteRequest routeRequest = new RouteRequest("등원A");
+            RouteRequest routeRequest = new RouteRequest("등원A", "board");
             academyService.save(academy);
             routeFacade.createRoute(authAcademy, routeRequest);
         }
     }
-
 }
