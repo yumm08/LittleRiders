33594c2c1e6fee0d3c6f743457062a79cb5d5cfb
김민준 <alswns1285@gmail.com>
2024-05-06 16:07:46 +0900

    Feat: [FE] 셔틀 정보 타입 추가
    S10P31A405-245

--- Frontend/src/types/shuttle/shuttle.ts
+++ Frontend/src/types/shuttle/shuttle.ts
@@ -1,3 +1,19 @@
 export interface DriveHistoryType {
   time: string
 }
+
+const SHUTTLE_STATUS = {
+  USE: 'USE',
+  REPAIRING: 'REPAIRING',
+  NOT_USE: 'NOT_USE',
+} as const
+export type ShuttleStatus = (typeof SHUTTLE_STATUS)[keyof typeof SHUTTLE_STATUS]
+export type AcademyShuttle = {
+  shuttleId: number
+  name: string
+  licenseNumber: string
+  type: string
+  imagePath: string
+  terminalNumber: string
+  status: ShuttleStatus
+}
