ab4d66119b299add6e28ca1ce6ef942edf549881
김민준 <alswns1285@gmail.com>
2024-05-06 16:10:25 +0900

    Feat: [FE] 홈페이지 구현
    S10P31A405-245

--- Frontend/src/pages/HomePage/index.tsx
+++ Frontend/src/pages/HomePage/index.tsx
@@ -1,3 +1,52 @@
+import { useEffect, useState } from 'react'
+
+import ChildList from '@pages/ChildPage/ChildList'
+
+import RealTimeMap from '@components/Main/RealTimeMap'
+import ShuttleInfo from '@components/Main/ShuttleInfo'
+import Spacing from '@components/Shared/Spacing'
+
+import { useFetchShuttleList } from '@hooks/shuttle'
+
+import Page from '@layouts/Page'
+import { AcademyShuttle } from '@types'
+
 export default function HomePage() {
-  return <div>HomePage</div>
+  const [selectedShuttle, setSelectedShuttle] = useState<AcademyShuttle>(null!)
+  const { shuttleList, isLoading } = useFetchShuttleList()
+
+  const handleShuttleButtonClick = (shuttle: AcademyShuttle) => {
+    setSelectedShuttle(shuttle)
+  }
+
+  useEffect(() => {
+    if (!isLoading && shuttleList) {
+      setSelectedShuttle(shuttleList[0])
+    }
+  }, [isLoading, shuttleList])
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <>
+      <Spacing style="h-[120px]" />
+
+      <div className="flex h-1/2 w-full border-b-2">
+        {selectedShuttle && <ShuttleInfo selectedShuttle={selectedShuttle} />}
+        {selectedShuttle && (
+          <RealTimeMap
+            shuttleList={shuttleList}
+            selectedShuttle={selectedShuttle}
+            onSelect={handleShuttleButtonClick}
+          />
+        )}
+      </div>
+
+      <Page>
+        <ChildList />
+      </Page>
+    </>
+  )
 }
