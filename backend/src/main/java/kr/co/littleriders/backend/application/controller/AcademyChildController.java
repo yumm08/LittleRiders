6556e28a1ed655e0c7b31b4c875bcda9099879f0
김도현 <nanayagoon@daum.net>
2024-05-08 19:31:16 +0900

    HotFix: [BE] 필요없는 엔드포인트 제거

--- backend/src/main/java/kr/co/littleriders/backend/application/controller/AcademyChildController.java
+++ backend/src/main/java/kr/co/littleriders/backend/application/controller/AcademyChildController.java
@@ -1,21 +1,17 @@
 package kr.co.littleriders.backend.application.controller;
 
 import kr.co.littleriders.backend.application.dto.request.AcademyChildUpdateRequest;
+import kr.co.littleriders.backend.application.dto.request.CreateAcademyChildRequest;
 import kr.co.littleriders.backend.application.dto.response.AcademyChildDetailResponse;
 import kr.co.littleriders.backend.application.dto.response.AcademyChildResponse;
-import kr.co.littleriders.backend.application.dto.response.PendingListResponse;
 import kr.co.littleriders.backend.application.facade.AcademyChildFacade;
 import kr.co.littleriders.backend.global.auth.annotation.Auth;
 import kr.co.littleriders.backend.global.auth.dto.AuthAcademy;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/academy/child")
@@ -24,11 +20,13 @@ public class AcademyChildController {
 
     private final AcademyChildFacade academyChildFacade;
 
+
+
     @GetMapping
     public ResponseEntity<List<AcademyChildResponse>> getAcademyChildList(@Auth AuthAcademy authAcademy) {
 
         Long academyId = authAcademy.getId();
-        List<AcademyChildResponse> academyChildList = academyChildFacade.readAcademyChildList(academyId);
+        List<AcademyChildResponse> academyChildList = academyChildFacade.getAcademyChildListByAcademyId(academyId);
 
         return ResponseEntity.ok().body(academyChildList);
     }
@@ -38,31 +36,33 @@ public class AcademyChildController {
                                                                             @PathVariable(value = "academyChildId") Long academyChildId) {
 
         Long academyId = authAcademy.getId();
-        AcademyChildDetailResponse academyChildDetailResponse = academyChildFacade.readAcademyChildDetail(academyId, academyChildId);
+        AcademyChildDetailResponse academyChildDetailResponse = academyChildFacade.getAcademyChildDetail(academyId, academyChildId);
 
         return ResponseEntity.ok().body(academyChildDetailResponse);
     }
 
-    @GetMapping("/{childHistoryId}/image")
-    public ResponseEntity<Resource> getAcademyChildImage(@Auth AuthAcademy authAcademy,
-                                                         @PathVariable(value = "childHistoryId") Long childHistoryId) {
 
-        Long academyId = authAcademy.getId();
-
-        Map<String, Object> image = academyChildFacade.readAcademyChildImage(academyId, childHistoryId);
-
-        Resource imageResource = (Resource) image.get("resource");
-        MediaType mediaType = (MediaType) image.get("mediaType");
-
-        HttpHeaders headers = new HttpHeaders();
-        if (mediaType != null) {
-            headers.setContentType(mediaType);
-        } else {
-            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-        }
-
-        return ResponseEntity.ok().headers(headers).body(imageResource);
-    }
+    //TODO - HOTFIX-이윤지 필요없음(김도현)
+//    @GetMapping("/{childHistoryId}/image")
+//    public ResponseEntity<Resource> getAcademyChildImage(@Auth AuthAcademy authAcademy,
+//                                                         @PathVariable(value = "childHistoryId") Long childHistoryId) {
+//
+//        Long academyId = authAcademy.getId();
+//
+//        Map<String, Object> image = academyChildFacade.readAcademyChildImage(academyId, childHistoryId);
+//
+//        Resource imageResource = (Resource) image.get("resource");
+//        MediaType mediaType = (MediaType) image.get("mediaType");
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        if (mediaType != null) {
+//            headers.setContentType(mediaType);
+//        } else {
+//            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+//        }
+//
+//        return ResponseEntity.ok().headers(headers).body(imageResource);
+//    }
 
     @PutMapping("/{academyChildId}")
     public ResponseEntity<Long> editAcademyChild(@Auth AuthAcademy authAcademy,
@@ -76,33 +76,14 @@ public class AcademyChildController {
     }
 
 
-    @GetMapping("/pending")
-    public ResponseEntity<List<PendingListResponse>> getPendingList(@Auth AuthAcademy authAcademy) {
+    //TODO - HOTFIX-이윤지 수정 필요 - 학원 아이 등록
+    @PostMapping("")
+    public ResponseEntity<Void> createAcademyChild(@Auth AuthAcademy authAcademy, @RequestBody CreateAcademyChildRequest createAcademyChildRequest) {
 
         Long academyId = authAcademy.getId();
-        List<PendingListResponse> pendingList = academyChildFacade.readPendingList(academyId);
-
-        return ResponseEntity.ok().body(pendingList);
-    }
-
-    @PostMapping("/pending")
-    public ResponseEntity<Void> addAcademyChild(@Auth AuthAcademy authAcademy
-                                                , @RequestBody List<Long> pendingList) {
-
-        Long academyId = authAcademy.getId();
-        academyChildFacade.insertAcademyChildList(academyId, pendingList);
-
+        academyChildFacade.createAcademyChild(academyId, createAcademyChildRequest);
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/pending")
-    public ResponseEntity<Void> removePendingList(@Auth AuthAcademy authAcademy
-                                                , @RequestBody List<Long> pendingList) {
-
-        Long academyId = authAcademy.getId();
-        academyChildFacade.deletePendingList(academyId, pendingList);
-
-        return ResponseEntity.ok().build();
-    }
 
 }
