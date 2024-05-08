b2db9e113164f1532d01f2718ad5879e73693a1c
김도현 <nanayagoon@daum.net>
2024-05-08 19:37:52 +0900

    HotFix: [BE] Family 제거

--- backend/src/main/java/kr/co/littleriders/backend/application/dto/request/ChildRegistRequest.java
+++ backend/src/main/java/kr/co/littleriders/backend/application/dto/request/ChildRegistRequest.java
@@ -1,14 +1,10 @@
 package kr.co.littleriders.backend.application.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import kr.co.littleriders.backend.domain.child.entity.Child;
-import kr.co.littleriders.backend.domain.family.entity.Family;
-import kr.co.littleriders.backend.global.entity.Gender;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,8 +26,5 @@ public class ChildRegistRequest {
 
 	private MultipartFile image;
 
-	public Child toEntity(Family family) {
-		return Child.of(this.name, this.birthDate, Enum.valueOf(Gender.class, this.getGender()), family);
-	}
 
 }
