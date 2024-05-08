9fe087660671b333476b73b1337da0d5588aecbf
김도현 <nanayagoon@daum.net>
2024-05-08 19:31:01 +0900

    HotFix: [BE] 필드 수정

--- backend/src/main/java/kr/co/littleriders/backend/domain/academy/entity/AcademyChild.java
+++ backend/src/main/java/kr/co/littleriders/backend/domain/academy/entity/AcademyChild.java
@@ -1,15 +1,15 @@
 package kr.co.littleriders.backend.domain.academy.entity;
 
 import jakarta.persistence.*;
-import kr.co.littleriders.backend.domain.child.entity.Child;
 import kr.co.littleriders.backend.domain.routeinfo.entity.ChildBoardDropInfo;
+import kr.co.littleriders.backend.global.entity.Gender;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.data.annotation.CreatedDate;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -25,47 +25,57 @@ public class AcademyChild {
     @Column(name = "id")
     private Long id; // 원생 정보 id
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "child_id", nullable = false)
-    private Child child; // 자녀
+    @Column(name = "name")
+    private String name;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "academy_family_id", nullable = false)
-    private AcademyFamily academyFamily; // 보호자 정보
+    @Column(name = "address")
+    private String address;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "academy_id", nullable = false)
-    private Academy academy; // 학원
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender")
+    private Gender gender;
+
+    @Column(name = "imagePath")
+    private String imagePath;
+
+    @Column(name = "beacon_number")
+    private String beaconNumber; // 카드 정보
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
 
     @Enumerated(EnumType.STRING)
     @Column(name = "status")
     private AcademyChildStatus status; // 상태
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "card_type", nullable = false)
-    private CardType cardType; // 카드 종류
 
-    @Column(name = "card_number")
-    private String cardNumber; // 카드 정보
+    @Column(name = "memo")
+    private String memo;
 
-    @LastModifiedDate
-    @DateTimeFormat(pattern = "yyyy-MM-dd/HH:mm:ss")
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt; // 상태 변경 일자
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
 
     @OneToMany(mappedBy = "academyChild")
     private List<ChildBoardDropInfo> childBoardDropInfoList; // 원생 승하차 목록
 
-    private AcademyChild(Child child, Academy academy, AcademyFamily family, AcademyChildStatus status, CardType type) {
-        this.child = child;
-        this.academy = academy;
-        this.academyFamily = family;
-        this.status = status;
-        this.cardType = type;
-    }
 
-    public static AcademyChild of(Child child, Academy academy, AcademyFamily family, AcademyChildStatus status, CardType type) {
-        return new AcademyChild(child, academy, family, status, type);
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "academy_id", nullable = false)
+    private Academy academy; // 학원
+
+
+    //TODO- HOTFIX-이윤지 -
+    //TODO- HOTFIX-이수현 - null 다른값 처리 필요함
+
+    public static AcademyChild of(Academy academy, AcademyChildStatus status) {
+
+        return null;
     }
 
     public void updateStatus(AcademyChildStatus status) {
@@ -76,11 +86,8 @@ public class AcademyChild {
         return this.academy.equals(academy);
     }
 
-	public boolean isAttending() {
+    public boolean isAttending() {
         return this.status.equals(AcademyChildStatus.ATTENDING);
-	}
-
-    public boolean isFamilyAvail() {
-        return this.getAcademyFamily().getStatus().equals(AcademyFamilyStatus.AVAIL);
     }
+
 }
