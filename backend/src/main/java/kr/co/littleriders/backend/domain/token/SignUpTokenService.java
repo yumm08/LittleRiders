c1897d6ab0d7d315db2415e05803554706a2749c
김도현 <nanayagoon@daum.net>
2024-05-08 19:33:04 +0900

    HotFix: [BE] Family 제거

--- backend/src/main/java/kr/co/littleriders/backend/domain/token/SignUpTokenService.java
+++ backend/src/main/java/kr/co/littleriders/backend/domain/token/SignUpTokenService.java
@@ -5,8 +5,6 @@ import kr.co.littleriders.backend.domain.token.entity.SignUpToken;
 public interface SignUpTokenService {
 
 
-    SignUpToken findFamilySignUpTokenByEmailAndToken(String email, String token);
-
     void save(SignUpToken signUpToken);
 
     void delete(SignUpToken signUpToken);
