9809b48bc72ccbd9e4dfbe1d8a8c7895b6045a10
김도현 <nanayagoon@daum.net>
2024-05-08 18:39:15 +0900

    HotFix: [BE] AcademyFamily중 필요없는 관련 클래스 삭제

--- backend/src/main/java/kr/co/littleriders/backend/domain/academy/service/AcademyChildCustomRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.co.littleriders.backend.domain.academy.service;
-
-import kr.co.littleriders.backend.domain.academy.entity.AcademyFamily;
-
-interface AcademyChildCustomRepository {
-
-    boolean existsByAcademyFamilyAndAttending(AcademyFamily academyFamily);
-}
