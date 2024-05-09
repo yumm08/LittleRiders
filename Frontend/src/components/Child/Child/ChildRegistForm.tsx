58f6fcda676ee9c30d429e2292684cb5708cfad2
김민준 <alswns1285@gmail.com>
2024-05-10 02:49:50 +0900

    Feat: [FE] 원생 등록 구현
    S10P31A405-322

--- Frontend/src/components/Child/Child/ChildRegistForm.tsx
+++ Frontend/src/components/Child/Child/ChildRegistForm.tsx
@@ -1,14 +1,22 @@
 import ChildRegistFormBody from '@components/Child/Child/ChildRegistFormBody'
 import ChildRegistFormFooter from '@components/Child/Child/ChildRegistFormFooter'
 
-import { ChildRegistInfo } from '@types'
+import { usePostChild } from '@hooks/child'
+
+import { ChildRegistFormInfo } from '@types'
 import { FormProvider, useForm } from 'react-hook-form'
 
 export default function ChildRegistForm() {
-  const methodList = useForm<ChildRegistInfo>({ mode: 'onBlur' })
+  const methodList = useForm<ChildRegistFormInfo>({ mode: 'onBlur' })
+  const { registChild } = usePostChild()
 
-  const onSubmit = (childRegistInfo: ChildRegistInfo) => {
-    console.log(childRegistInfo)
+  const onSubmit = (childRegistFormInfo: ChildRegistFormInfo) => {
+    const formattedFile = childRegistFormInfo.image
+      ? childRegistFormInfo.image[0]
+      : null
+    const childRegistInfo = { ...childRegistFormInfo, image: formattedFile }
+
+    registChild(childRegistInfo)
   }
 
   return (
