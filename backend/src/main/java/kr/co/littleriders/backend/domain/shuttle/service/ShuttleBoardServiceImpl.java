c84a94020f9fab7bc2923dbec6b935c69f1c018a
김도현 <nanayagoon@daum.net>
2024-05-10 01:10:44 +0900

    Feat: [BE] 승차관련 정보 조회 및 삭제 구현
    S10P31A405-327

---
+++ backend/src/main/java/kr/co/littleriders/backend/domain/shuttle/service/ShuttleBoardServiceImpl.java
@@ -0,0 +1,52 @@
+package kr.co.littleriders.backend.domain.shuttle.service;
+
+
+import kr.co.littleriders.backend.domain.shuttle.ShuttleBoardService;
+import kr.co.littleriders.backend.domain.shuttle.entity.ShuttleBoard;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+class ShuttleBoardServiceImpl implements ShuttleBoardService {
+
+    private final ShuttleBoardRepository shuttleBoardRepository;
+    @Override
+    public ShuttleBoard findByAcademyChildId(long academyChildId) {
+        return shuttleBoardRepository.findById(academyChildId).orElseThrow();
+    }
+
+    @Override
+    public List<ShuttleBoard> findByShuttleId(long shuttleId) {
+        return shuttleBoardRepository.findByShuttleId(shuttleId);
+    }
+
+    @Override
+    public List<ShuttleBoard> findByAcademyId(long academyId) {
+        return shuttleBoardRepository.findByAcademyId(academyId);
+    }
+
+    @Override
+    public void deleteAllByShuttleId(long shuttleId) {
+        List<ShuttleBoard> ShuttleBoardList = shuttleBoardRepository.findByShuttleId(shuttleId);
+        shuttleBoardRepository.deleteAll(ShuttleBoardList);
+    }
+
+    @Override
+    public void deleteAllByAcademyId(long academyId) {
+        List<ShuttleBoard> ShuttleBoardList  =shuttleBoardRepository.findByAcademyId(academyId);
+        shuttleBoardRepository.deleteAll(ShuttleBoardList);
+    }
+
+    @Override
+    public void delete(ShuttleBoard shuttleBoard) {
+        shuttleBoardRepository.delete(shuttleBoard);
+    }
+
+    @Override
+    public void save(ShuttleBoard shuttleBoard) {
+        shuttleBoardRepository.save(shuttleBoard);
+    }
+}
