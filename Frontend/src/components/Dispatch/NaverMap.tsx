dc6cf3f3d069897f3ae1da023066d111134d0b0b
gwanu-dev <devkwanwoo@gmail.com>
2024-05-09 21:34:13 +0900

    Feat: [FE] 네이버 맵 관련 함수 구조 변경 및 노선 관리 페이지 구조 변경
    S10P31A405-245

--- Frontend/src/components/Dispatch/NaverMap.tsx
+++ Frontend/src/components/Dispatch/NaverMap.tsx
@@ -1,8 +1,57 @@
-/**
- *
- * @summary Route 페이지에서 사용되는 NaverMap
- * @returns
- */
-export default function NaverMap() {
-  return <div id="map" className="h-550 w-7/12 max-2xl:w-full"></div>
+import { useEffect } from 'react'
+
+import { MapHook } from '@hooks/map'
+
+import MapOverlay from './MapOverlay'
+
+import { BASE_LAT, BASE_LNG } from '@constants'
+
+interface Props {
+  mapRef: React.RefObject<naver.maps.Map>
+  mapDiv: React.RefObject<HTMLDivElement>
+  selectedRouteId: number
+  isOverlayOpen: boolean
+  handleAddButton: () => void
+  handleCloseButton: () => void
+}
+
+export default function NaverMap({
+  mapDiv,
+  mapRef,
+  selectedRouteId,
+  isOverlayOpen,
+  handleAddButton,
+  handleCloseButton,
+}: Props) {
+  const { initMap } = MapHook(mapRef)
+  useEffect(() => {
+    initMap(mapDiv, {
+      center: new naver.maps.LatLng(BASE_LAT, BASE_LNG),
+      zoom: 15,
+      minZoom: 7,
+      zoomControl: false,
+      disableKineticPan: true,
+      disableDoubleClickZoom: true,
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return (
+    <div
+      id="map"
+      className={`flex max-h-full w-full items-center justify-center transition-all duration-500 ease-in-out max-2xl:w-full ${selectedRouteId === -1 ? 'pr-0' : 'mr-[674px]'}`}
+      ref={mapDiv}
+    >
+      {isOverlayOpen && (
+        <>
+          <img
+            className="absolute z-10 mb-8 self-center transition-all ease-in-out hover:mb-12 hover:size-14"
+            src="/src/assets/image/map-center-icon.svg"
+            width={40}
+            onClick={handleAddButton}
+          />
+          <MapOverlay mapRef={mapRef} handleCloseButton={handleCloseButton} />
+        </>
+      )}
+    </div>
+  )
 }
