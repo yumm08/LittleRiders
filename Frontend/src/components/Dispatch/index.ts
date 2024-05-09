50d0bf9122bf82f63262e2cc5b9198ec2eb5e212
gwanu-dev <devkwanwoo@gmail.com>
2024-05-09 21:33:07 +0900

    Ref: [FE] 컴포넌트 이름 컨벤션을 위해 구조 변경
    S10P31A405-245

--- Frontend/src/components/Dispatch/index.ts
+++ Frontend/src/components/Dispatch/index.ts
@@ -1,3 +1,3 @@
-export { default as ItemListView } from '@components/Dispatch/ItemListView'
+export { default as RouteDetailSlide } from '@components/Dispatch/RouteDetailSlide'
 export { default as NaverMap } from '@components/Dispatch/NaverMap'
 export { default as RouteList } from '@components/Dispatch/RouteList'
