132cdf73a059824dbe3c9943ad87779dd2d61c3d
김도현 <nanayagoon@daum.net>
2024-05-10 14:36:15 +0900

    Feat: [DEVICE] API 서버 연동
    S10P31A405-261

--- device/APIFetch.py
+++ device/APIFetch.py
@@ -35,126 +35,113 @@ class APIFetcher:
         self.cookies = {'refresh-token': response.cookies["refresh-token"]}
 
 
-    def getStationListByRouteId(self,id) -> list:
-        print("call here")
-
-        url = f"{self.BASE_URL}/shuttle/route/{id}"
-
-        
-        itemList= [ {"id":1,"name":"강남역","latitude": 37.4979084,"longitude":127.0276954 },
-            {"id":2,"name":"언주역","latitude": 37.5071434,"longitude":127.0340362 },
-            {"id":3,"name":"역삼역","latitude": 37.5006407,"longitude":127.0370402  },
-            {"id":4,"name":"선릉역","latitude": 37.5044794,"longitude":127.0489385  },
-            {"id":5,"name":"신논현역", "latitude":37.5044369, "longitude":127.0246162},
-            {"id":6,"name":"논현역", "latitude":37.5109732, "longitude":127.0214405  },
-            {"id":7,"name":"학동역", "latitude":37.5142071, "longitude":127.0316544  },
-            {"id":8,"name":"반포역", "latitude":37.5079604, "longitude":127.0116558  },
-            {"id":9,"name":"교대역 1번출구", "latitude":37.4940609,"longitude": 127.0156148 },
-            {"id":10,"name":"교대역 9번출구", "latitude":37.4932948,"longitude": 127.013147 }
-            ]
-        
-        result = random.sample(itemList,random.randint(1,len(itemList)))
-        for index,i in enumerate(result):
-            i["visitOrder"]=index+1
-        
-    
-        return result
-    
-    def _getRandomStation(self) -> list:
-        stationList = [ {"id":1,"name":"강남역","latitude": 37.4979084,"longitude":127.0276954 },
-            {"id":2,"name":"언주역","latitude": 37.5071434,"longitude":127.0340362 },
-            {"id":3,"name":"역삼역","latitude": 37.5006407,"longitude":127.0370402  },
-            {"id":4,"name":"선릉역","latitude": 37.5044794,"longitude":127.0489385  },
-            {"id":5,"name":"신논현역", "latitude":37.5044369, "longitude":127.0246162},
-            {"id":6,"name":"논현역", "latitude":37.5109732, "longitude":127.0214405  },
-            {"id":7,"name":"학동역", "latitude":37.5142071, "longitude":127.0316544  },
-            {"id":8,"name":"반포역", "latitude":37.5079604, "longitude":127.0116558  },
-            {"id":9,"name":"교대역 1번출구", "latitude":37.4940609,"longitude": 127.0156148 },
-            {"id":10,"name":"교대역 9번출구", "latitude":37.4932948,"longitude": 127.013147 }
-            ]
-        choiced = list(random.sample(stationList,random.randint(1,len(stationList))))
-
-        return [{'visitOrder': index+1, **i} for index, i in enumerate(choiced)]
-
-
-
 
     def getRouteList(self) -> list:
-    
-        url = f"{self.BASE_URL}/shuttle/route"
-
-        
-
-        return [
-                {"id":1,"name":"등원A","stationList": self._getRandomStation()},
-                {"id":2,"name":"등원B","stationList":self._getRandomStation()},
-                {"id":3,"name":"등원C","stationList":self._getRandomStation()},
-                {"id":4,"name":"등원D","stationList":self._getRandomStation()},
-                {"id":5,"name":"등원E","stationList":self._getRandomStation()},
-                {"id":6,"name":"등원F","stationList":self._getRandomStation()},
-                {"id":7,"name":"등원G","stationList":self._getRandomStation()},
-                {"id":8,"name":"등원H","stationList":self._getRandomStation()},
-                {"id":9,"name":"등원I","stationList":self._getRandomStation()},
-                {"id":10,"name":"등원J","stationList":self._getRandomStation()},
-                {"id":11,"name":"등원K","stationList":self._getRandomStation()},
-                {"id":12,"name":"등원!","stationList":self._getRandomStation()},
-                {"id":13,"name":"등원2","stationList":self._getRandomStation()},
-                {"id":14,"name":"등원3","stationList":self._getRandomStation()},
-                {"id":15,"name":"등원4","stationList":self._getRandomStation()},
-                {"id":16,"name":"등원5","stationList":self._getRandomStation()},
-                {"id":17,"name":"등원6","stationList":self._getRandomStation()},
-                {"id":18,"name":"등원7","stationList":self._getRandomStation()},
-                {"id":19,"name":"등원8","stationList":self._getRandomStation()},
-                {"id":20,"name":"등원9","stationList":self._getRandomStation()}
-                
-            ]
-        
-
+        url = f"{self.BASE_URL}/shuttle/route/station"
         response = requests.get(url,headers=self.headers)
-        if(response.status_code == 400):
+        if(response.status_code == 401):
             self._reIssue()
             return self.getRouteList()
-        
+        return response.json()   
+    
+
+    def getRouteDetail(self,id) -> list:
+        url = f"{self.BASE_URL}/shuttle/route/{id}"
+        response = requests.get(url,headers=self.headers)
+        if(response.status_code == 401):
+            self._reIssue()
+            return self.getRouteDetail()
         return response.json()
+
+        
         
     def uploadPosition(self,position :RMCPosition) -> None:
         url = f"{self.BASE_URL}/shuttle/location"
         data = position.toJson()
         
         response = requests.post(url,headers=self.headers,json=data)
-        print(response.status_code)
-        if(response.status_code != 201):
+        if(response.status_code == 401):
             self._reIssue()
             return self.uploadPosition(position=position)
+        # return response.json()
         
-    def getImage(self):
-        image = requests.get("https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Python_and_Qt.svg/160px-Python_and_Qt.svg.png",stream=True)
+    def getImage(self,imagePath):
+        image = requests.get(f"{self.BASE_URL}/content/{imagePath}",stream=True)
         return image.raw.data
     
     def getDriverInfo(self,uuid):
-
-        return {
-            "id" : 1,
-            "name" : "김수한무",
-            "phoneNumber" : "010-1234-5678"
-        }
+        url = f"{self.BASE_URL}/shuttle/tag/driver/{uuid}"
+        response = requests.get(url,headers=self.headers)
+        if(response.status_code == 401):
+            self._reIssue()
+            return self.getTeacherInfo(uuid)
+        print(response.json())
+        return response.json()
     
     def getTeacherInfo(self,uuid):
+        url = f"{self.BASE_URL}/shuttle/tag/teacher/{uuid}"
+        response = requests.get(url,headers=self.headers)
+        if(response.status_code == 401):
+            self._reIssue()
+            return self.getTeacherInfo(uuid)
+        print(response.json())
+        return response.json()
+    
 
-        return {
-            "id" : 1,
-            "name" : "박사장",
-            "phoneNumber" : "010-5555-5678"
+    def getEndDrive(self):
+        url = f"{self.BASE_URL}/shuttle/end"
+        response = requests.post(url,headers=self.headers)
+        if(response.status_code == 401):
+            self._reIssue()
+            return self.getEndDrive()
+        return response.json()
+    
+
+    def getStartDrive(self,routeId,driverId,teacherId):
+        url = f"{self.BASE_URL}/shuttle/start"
+        data = {
+            "routeId":routeId,
+            "driverId":driverId,
+            "teacherId":teacherId
         }
+        response = requests.post(url,headers=self.headers,json=data)
+        if(response.status_code == 401):
+            self._reIssue()
+            return self.getStartDrive(routeId,driverId,teacherId)
+        
+        print(response)
+        
+        # return response.json()
 
-        #return requests.get(f"{self.BASE_URL}/shuttle/tag/driver?uuid={uuid}").json()
-        #if(response.status_code == 404):
-        #    self._reIssue()
-        #    return self.uploadPosition(position=position)
 
+    def getBoard(self,beaconUUID,latitude,longitude):
+        data ={
+            "beaconUUID" : beaconUUID,
+            "latitude" : latitude,
+            "longitude" : longitude
+        }
+        url = f"{self.BASE_URL}/shuttle/child/board"
+        response = requests.post(url,headers=self.headers,json=data)
+        print(response)
+        if(response.status_code == 401):
+            self._reIssue()
+            return self.getBoard(beaconUUID,latitude,longitude)
+        
+    def getDrop(self,beaconUUID,latitude,longitude):
+        data ={
+            "beaconUUID" : beaconUUID,
+            "latitude" : latitude,
+            "longitude" : longitude
+        }
+        url = f"{self.BASE_URL}/shuttle/child/drop"
+        response = requests.post(url,headers=self.headers,json=data)
+        print(response)
+        if(response.status_code == 401):
+            self._reIssue()
+            return self.getBoard(beaconUUID,latitude,longitude)
 
 if __name__ == "__main__":
     apiFetcher = APIFetcher("")
-    print(apiFetcher.getRouteList())
+
 
 
