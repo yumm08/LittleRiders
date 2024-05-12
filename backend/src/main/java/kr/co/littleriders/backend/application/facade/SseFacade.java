92c19c2f1ff90b6ceb92dd73da6948e31a86138c
김도현 <nanayagoon@daum.net>
2024-05-12 14:25:01 +0900

    Feat: [BE] 승차 하차정보에 대한 sse 이벤트 구현
    S10P31A405-314

--- backend/src/main/java/kr/co/littleriders/backend/application/facade/SseFacade.java
+++ backend/src/main/java/kr/co/littleriders/backend/application/facade/SseFacade.java
@@ -1,6 +1,7 @@
 package kr.co.littleriders.backend.application.facade;
 
 import kr.co.littleriders.backend.application.dto.request.ShuttleLocationRequest;
+import kr.co.littleriders.backend.domain.academy.entity.AcademyChild;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 public interface SseFacade {
@@ -10,4 +11,8 @@ public interface SseFacade {
     SseEmitter createAcademySseConnectionByAcademyId(long academyId);
 
     void broadcastShuttleLocationByShuttleId(long shuttleId, ShuttleLocationRequest shuttleLocationRequest);
+
+    void broadcastBoardByAcademyIdAndViewerId(long academyId, String viewerUuid, AcademyChild academyChild, double latitude, double longitude);
+
+    void broadcastDropByAcademyIdAndViewerId(long academyId, String viewerUuid, AcademyChild academyChild, double latitude, double longitude);)
 }
