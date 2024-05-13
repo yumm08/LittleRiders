838c00240c33d60df4a1d98f8cc87babc087cc3c
이수현 <suhyeon9120@gmail.com>
2024-05-13 14:25:56 +0900

    Fix: [BE] 에러 코드 타입 수정
    S10P31A405-344
    
    RouteErrorCode -> RouteStationErrorCode 으로 수정하였습니다.

--- backend/src/main/java/kr/co/littleriders/backend/domain/routeinfo/error/exception/RouteStationException.java
+++ backend/src/main/java/kr/co/littleriders/backend/domain/routeinfo/error/exception/RouteStationException.java
@@ -1,18 +1,18 @@
 package kr.co.littleriders.backend.domain.routeinfo.error.exception;
 
-import kr.co.littleriders.backend.domain.route.error.code.RouteErrorCode;
+import kr.co.littleriders.backend.domain.routeinfo.error.code.RouteStationErrorCode;
 import kr.co.littleriders.backend.global.error.exception.LittleRidersException;
 import lombok.Getter;
 
 @Getter
 public final class RouteStationException extends LittleRidersException {
     
-    RouteStationException(RouteErrorCode errorCode) {
+    RouteStationException(RouteStationErrorCode errorCode) {
         super(errorCode);
 
     }
 
-    public static RouteStationException from(RouteErrorCode errorCode) {
+    public static RouteStationException from(RouteStationErrorCode errorCode) {
         return new RouteStationException(errorCode);
     }
 
