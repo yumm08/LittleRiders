2a9431eee1b4a349ab94071d7d669fd9c3ef6e8b
김민준 <alswns1285@gmail.com>
2024-05-13 16:52:55 +0900

    Feat: [FE] polyline 초기화 기능 구현
    S10P31A405-317

--- Frontend/src/hooks/main/realTimeMap.ts
+++ Frontend/src/hooks/main/realTimeMap.ts
@@ -1,6 +1,5 @@
 import { useRef, useState } from 'react'
 
-import COLOR_PALETTE from '@style/ColorPalette'
 import { LocationInfo } from '@types'
 
 const DEFAULT_OPTIONS = {
@@ -13,8 +12,7 @@ const DEFAULT_OPTIONS = {
 export const useSetRealTimeMap = () => {
   const [realTimeMap, setRealTimeMap] = useState<naver.maps.Map>()
   const [realTimeMarker, setRealTimeMarker] = useState<naver.maps.Marker>()
-  const [polyline, setPolyline] = useState<naver.maps.Polyline>()
-  const [polylineList, setPolylineList] = useState<naver.maps.Polyline[]>([])
+  const polyline = useRef<naver.maps.Polyline>()
   const latLngList = useRef<naver.maps.LatLng[]>([])
 
   /**
@@ -69,9 +67,12 @@ export const useSetRealTimeMap = () => {
    *
    * @param curLocationInfo 현재 위치 정보
    */
-  const drawRealTimeMarker = (curLocationInfo: LocationInfo) => {
+  const drawRealTimeMarker = (
+    curLocationInfo: LocationInfo,
+    map: naver.maps.Map,
+  ) => {
     if (!realTimeMap) {
-      return
+      setRealTimeMap(map)
     }
 
     if (realTimeMarker) {
@@ -89,7 +90,7 @@ export const useSetRealTimeMap = () => {
 
       const marker = new naver.maps.Marker({
         position,
-        map: realTimeMap,
+        map,
         icon: {
           content: `<div id="marker" style="transform:translate(-25px, -25px);width:50px;height:50px"><img src="/bus.svg" style="width:50px; height:50px;" /></div>`,
         },
@@ -100,52 +101,16 @@ export const useSetRealTimeMap = () => {
   }
 
   /**
-   * 전의 위치와 현재 위치를 기반으로 두 위치를 잇는 polyline을 그리는 함수
-   *
-   * @param prevLocationInfo 전의 위치 정보
-   * @param curLocationInfo 현재 위치 정보
-   */
-  const drawPolyline = (
-    prevLocationInfo: LocationInfo,
-    curLocationInfo: LocationInfo,
-  ) => {
-    if (!realTimeMap) {
-      return
-    }
-
-    const path = [
-      new naver.maps.LatLng(
-        prevLocationInfo.latitude,
-        prevLocationInfo.longitude,
-      ),
-      new naver.maps.LatLng(
-        curLocationInfo.latitude,
-        curLocationInfo.longitude,
-      ),
-    ]
-
-    const newPolyLine = new naver.maps.Polyline({
-      path: path,
-      strokeColor: COLOR_PALETTE.darkgreen,
-      strokeWeight: 10,
-      strokeOpacity: 0.7,
-      strokeLineCap: 'round',
-    })
-
-    newPolyLine.setMap(realTimeMap)
-
-    setPolyline(newPolyLine)
-    setPolylineList((prev) => [...prev, newPolyLine])
-  }
-
-  /**
    * LatLng 리스트에 있는 LatLng으로 한번에 Polyline을 그리는 함수
    *
    * @param curLocationInfo
    */
-  const drawPolylineWithList = (curLocationInfo: LocationInfo) => {
+  const drawPolylineWithList = (
+    curLocationInfo: LocationInfo,
+    map: naver.maps.Map,
+  ) => {
     if (!realTimeMap) {
-      return
+      setRealTimeMap(map)
     }
 
     const curLatLng = new naver.maps.LatLng(
@@ -155,53 +120,30 @@ export const useSetRealTimeMap = () => {
 
     if (polyline) {
       latLngList.current.push(curLatLng)
-      polyline.getPath().push(curLatLng)
-    } else {
-      const polyline = new naver.maps.Polyline({
-        map: realTimeMap,
-        path: latLngList.current,
-        clickable: true,
-        strokeColor: '#5347AA',
-        strokeOpacity: 0.5,
-        strokeWeight: 5,
-        strokeLineCap: 'round',
-      })
-      setPolyline(polyline)
+      polyline.current?.getPath().push(curLatLng)
     }
   }
 
-  /**
-   * Polyline을 보이게 하는 함수
-   */
-  const showPolyline = () => {
-    if (!realTimeMap) {
-      return
-    }
-
-    polylineList.forEach((polyline) => {
-      polyline.setMap(realTimeMap)
+  const initPolyline = (map: naver.maps.Map) => {
+    const polylineT = new naver.maps.Polyline({
+      map,
+      path: [],
+      clickable: true,
+      strokeColor: '#5347AA',
+      strokeOpacity: 0.5,
+      strokeWeight: 5,
+      strokeLineCap: 'round',
     })
-    polyline?.setMap(realTimeMap)
-  }
-
-  /**
-   * Polyline을 숨기는 함수
-   */
-  const hidePolyline = () => {
-    polylineList.forEach((polyline) => {
-      polyline.setMap(null)
-    })
-    polyline?.setMap(null)
+    polyline.current = polylineT
   }
 
   return {
     realTimeMap,
+    polyline,
     drawRealTimeMarker,
-    drawPolyline,
     initRealTimeMap,
-    hidePolyline,
-    showPolyline,
     drawPolylineWithList,
     setDirection,
+    initPolyline,
   }
 }
