22569edac3ee799407b6dddc3615f7eeabcf64f2
Gwanu <devkwanwoo@gmail.com>
2024-05-14 09:42:37 +0900

    !HOTFIX: [FE] 데이터 타입 Key값 변경으로 인한 버그 수정

--- Frontend/src/components/Child/Child/ChildCardList.tsx
+++ Frontend/src/components/Child/Child/ChildCardList.tsx
@@ -3,13 +3,13 @@ import ChildCard from '@components/Child/Child/ChildCard'
 import { ChildInfo } from '@types'
 
 interface Props {
-  academyChildList: ChildInfo[]
+  childList: ChildInfo[]
 }
 
-export default function ChildCardList({ academyChildList }: Props) {
+export default function ChildCardList({ childList }: Props) {
   return (
     <div className="grid grid-cols-6 gap-4">
-      {academyChildList.map((child: ChildInfo) => (
+      {childList.map((child: ChildInfo) => (
         <ChildCard key={child.academyChildId} childInfo={child} />
       ))}
     </div>
