336266d4cf0ccd7acc237eceb074f34fb26bfd1a
이수현 <suhyeon9120@gmail.com>
2024-05-14 15:25:52 +0900

    Ref: [BE] 불필요한 import 삭제
    S10P31A405-288

--- backend/src/main/java/kr/co/littleriders/backend/application/controller/RouteController.java
+++ backend/src/main/java/kr/co/littleriders/backend/application/controller/RouteController.java
@@ -14,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/academy/route")
