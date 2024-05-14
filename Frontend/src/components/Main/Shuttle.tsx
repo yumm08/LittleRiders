53e819c588c13d32fb1965fc5ad180029a3344ab
김민준 <alswns1285@gmail.com>
2024-05-14 22:14:25 +0900

    Feat: [FE] Init 정보 그리기
    S10P31A405-317
    
    - 이전의 정보들 polyline 그리기
    - 이전의 승차 정보들 마커 찍기
    - 이전의 하차 정보들 마커 찍기

--- Frontend/src/components/Main/Shuttle.tsx
+++ Frontend/src/components/Main/Shuttle.tsx
@@ -4,21 +4,23 @@ import { useQuery } from '@tanstack/react-query'
 
 import { useSetRealTimeMap } from '@hooks/main/realTimeMap'
 
-import { InitData, LocationInfo } from '@types'
+import { BoardInfo, InitData, InitDataLocationInfo, LocationInfo } from '@types'
 
 interface Props {
   shuttleId: number
   realTimeMap: naver.maps.Map
+  isSelected: boolean
 }
 
-export default function Shuttle({ shuttleId, realTimeMap }: Props) {
-  const curLocationInfo = useRef<LocationInfo>()
+export default function Shuttle({ shuttleId, realTimeMap, isSelected }: Props) {
+  const curLocationInfo = useRef<LocationInfo | InitDataLocationInfo>()
 
   const {
     drawRealTimeMarker,
     drawPolylineWithList,
     setDirection,
     initPolyline,
+    drawBoardMarker,
   } = useSetRealTimeMap()
 
   const { data: locationInfo } = useQuery<LocationInfo>({
@@ -34,13 +36,43 @@ export default function Shuttle({ shuttleId, realTimeMap }: Props) {
     if (initData) {
       const locationList = initData.locationList
 
+      if (locationList.length > 0) {
+        locationList.sort(
+          (a, b) => new Date(a.time).getTime() - new Date(b.time).getTime(),
+        )
+
+        const lastLocationInfo = locationList.at(-1) as InitDataLocationInfo
+        curLocationInfo.current = lastLocationInfo
+        drawRealTimeMarker(lastLocationInfo, realTimeMap)
+      }
+
       initPolyline(realTimeMap)
 
-      locationList
-        .sort((a, b) => new Date(a.time) - new Date(b.time))
-        .forEach((locationInfo) => {
-          drawPolylineWithList(locationInfo, realTimeMap)
-        })
+      locationList.forEach((locationInfo) => {
+        drawPolylineWithList(locationInfo, realTimeMap)
+      })
+    }
+  }, [initData])
+
+  // init 데이터가 있다면, board marker를 그린다
+  useEffect(() => {
+    if (initData) {
+      const boardList = initData.boardList
+
+      boardList.forEach((boardInfo: BoardInfo) => {
+        drawBoardMarker(boardInfo, realTimeMap)
+      })
+    }
+  }, [initData])
+
+  // init 데이터가 있다면, drop marker를 그린다
+  useEffect(() => {
+    if (initData) {
+      const boardList = initData.boardList
+
+      boardList.forEach((boardInfo: BoardInfo) => {
+        drawBoardMarker(boardInfo, realTimeMap)
+      })
     }
   }, [initData])
 
@@ -63,6 +95,17 @@ export default function Shuttle({ shuttleId, realTimeMap }: Props) {
       drawPolylineWithList(locationInfo, realTimeMap)
     }
   }, [locationInfo])
+
+  // 선택한 호차의 위치를 맵의 중심으로 한다
+  useEffect(() => {
+    if (isSelected && curLocationInfo.current) {
+      if (isSelected) {
+        const { latitude, longitude } = curLocationInfo.current
+        const latLng = new naver.maps.LatLng(latitude, longitude)
+        realTimeMap.setCenter(latLng)
+      }
+    }
+  }, [isSelected])
 
   return null
 }
