11991ffc1e947733cd65b8b700e037bd55753433
김민준 <alswns1285@gmail.com>
2024-05-16 17:48:17 +0900

    !HOTFIX: [FE] 빌드에러 해결

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -11,7 +11,7 @@ async function enableMocking() {
 
   return
 
-  const { worker } = await import('@mocks/browser')
+  // const { worker } = await import('@mocks/browser')
 
   // return worker.start()
 }
