2a2600632572c3fe64737dcf31ba5697faea6032
gwanu-dev <devkwanwoo@gmail.com>
2024-05-17 17:57:43 +0900

    Feat: [FE] 아이콘 추가 및 디자인 변경
    S10P31A405-355

--- FrontEnd/src/pages/Landing/index.tsx
+++ FrontEnd/src/pages/Landing/index.tsx
@@ -1,6 +1,9 @@
+import { useNavigate } from 'react-router-dom'
+
 export default function Landing() {
+  const navigate = useNavigate()
   return (
-    <div className="flex h-full w-full flex-row p-4">
+    <div className="flex h-full w-full flex-row border p-4">
       <div className="flex w-1/2 flex-col items-center justify-center ">
         <div className="m-2 h-1/2 w-5/6">
           <img className="h-full" src="/default.png"></img>
@@ -33,17 +36,43 @@ export default function Landing() {
         </div>
       </div>
       <div className="flex h-full w-1/2 flex-col items-center justify-between">
-        <button className="rounde m-2 h-1/2 w-5/6 border-2 p-3 text-2xl font-bold transition-colors ease-in-out active:bg-lightgreen active:text-slate-100">
-          운행하러가기
+        <button
+          className=" m-2 h-1/2 w-5/6 rounded border-4 border-lightgreen p-3 text-2xl font-bold transition-colors ease-in-out active:bg-lightgreen active:text-slate-100"
+          onClick={() => {
+            navigate('/route')
+          }}
+        >
+          <div className="flex h-full w-full items-center justify-between text-3xl font-extrabold">
+            <img src="/start.svg" className=" h-24" />
+            <p className="text-4xl">운행 하기</p>
+          </div>
         </button>
-        <button className="m-2 h-1/6 w-5/6 rounded border-2 border-darkgreen p-2 text-2xl font-bold transition-colors ease-in-out active:bg-lightgreen">
-          최근 이동 기록
+        <button
+          className="m-2 h-1/6 w-5/6 rounded border-4 border-blue-500 p-2 text-2xl font-bold transition-colors ease-in-out active:bg-blue-500"
+          onClick={() => {}}
+        >
+          <div className="flex h-full w-full items-center justify-between">
+            <img src="/history.svg" className=" h-12" />
+            최근 이동 기록
+          </div>
         </button>
-        <button className="m-2 h-1/6 w-5/6 rounded border-2 border-darkgreen  p-2 text-2xl font-bold transition-colors ease-in-out active:bg-lightgreen">
-          노선 보기
+        <button
+          className="m-2 h-1/6 w-5/6 rounded border-4 border-yellow p-2 text-2xl font-bold transition-colors ease-in-out active:bg-yellow"
+          onClick={() => {}}
+        >
+          <div className="flex h-full w-full items-center justify-between">
+            <img src="/route.svg" className=" h-12" />
+            노선 보기
+          </div>
         </button>
-        <button className="mt-2 h-1/6 w-5/6 rounded border-2 border-darkgreen  p-2 text-2xl font-bold transition-colors ease-in-out active:bg-lightgreen">
-          단말기 고유 정보
+        <button
+          className="mt-2 h-1/6 w-5/6 rounded border-4 border-slate-400 p-2 text-2xl font-bold transition-colors ease-in-out active:bg-slate-400"
+          onClick={() => {}}
+        >
+          <div className="flex h-full w-full items-center justify-between">
+            <img src="/terminal-info.svg" className=" h-12" />
+            단말기 고유 정보
+          </div>
         </button>
       </div>
     </div>
