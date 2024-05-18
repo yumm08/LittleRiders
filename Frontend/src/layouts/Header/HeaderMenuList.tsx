918520a507adf1f08b8fdd0585ccb7cf1907d95e
gwanu-dev <devkwanwoo@gmail.com>
2024-05-18 18:51:15 +0900

    !HOTFIX: header 디자인 변경

--- Frontend/src/layouts/Header/HeaderMenuList.tsx
+++ Frontend/src/layouts/Header/HeaderMenuList.tsx
@@ -3,7 +3,7 @@ import HeaderMenuListItem from '@layouts/Header/HeaderMenuListItem'
 
 export default function HeaderMenuList() {
   return (
-    <div className="flex h-2/5 w-full items-center gap-24 bg-lightgreen bg-opacity-70 px-[200px] shadow-inner ">
+    <div className="flex h-2/5 w-full items-center gap-24 px-[200px] ">
       {MENU_LIST.map(({ text, path }, index) => (
         <HeaderMenuListItem key={index} text={text} path={path} />
       ))}
