a687c379fc57fc666fe1bf32d56d199993f23d2c
gwanu-dev <devkwanwoo@gmail.com>
2024-05-18 15:15:24 +0900

    Design: [FE] Header 바 디자인 수정
    S10P31A405-345

--- Frontend/src/layouts/Header/HeaderMenuList.tsx
+++ Frontend/src/layouts/Header/HeaderMenuList.tsx
@@ -3,7 +3,7 @@ import HeaderMenuListItem from '@layouts/Header/HeaderMenuListItem'
 
 export default function HeaderMenuList() {
   return (
-    <div className="flex h-1/2 w-full items-center gap-24">
+    <div className="flex h-2/5 w-full items-center gap-24 bg-lightgreen bg-opacity-70 px-[200px] shadow-inner ">
       {MENU_LIST.map(({ text, path }, index) => (
         <HeaderMenuListItem key={index} text={text} path={path} />
       ))}
--- Frontend/src/layouts/Header/index.tsx
+++ Frontend/src/layouts/Header/index.tsx
@@ -3,7 +3,7 @@ import HeaderTop from '@layouts/Header/HeaderTop'
 
 export default function Header() {
   return (
-    <div className="fixed top-0 z-50 h-[120px] w-full bg-white px-[200px] font-bold shadow-md">
+    <div className="fixed top-0 z-50 h-[120px] w-full bg-white  font-bold shadow-md">
       <HeaderTop />
       <HeaderMenuList />
     </div>
