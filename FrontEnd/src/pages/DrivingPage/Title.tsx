5e7501785f93dcf97f5ad684cf77d372dd2dbbd5
gwanu-dev <devkwanwoo@gmail.com>
2024-05-19 16:25:51 +0900

    Design: [FE] 운행 종료 버튼 추가 및 디자인 세부 변경
    S10P31A405-354

--- FrontEnd/src/pages/DrivingPage/Title.tsx
+++ FrontEnd/src/pages/DrivingPage/Title.tsx
@@ -1,6 +1,6 @@
 export default function Title() {
   return (
-    <span className="flex  rounded-lg border-2 bg-white px-8 text-[25px] font-bold ">
+    <span className="flex rounded-lg border-2 bg-white px-8 text-[25px] font-bold ">
       운행 현황
     </span>
   )
